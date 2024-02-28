3f096ccbcd961f2b42044f87413d52635c9f211d
k.duplantier <k.duplantier@serma.com>
2024-02-28 07:48:04 -0500

    mutex fix

--- usb_can_bridge.cpp
+++ usb_can_bridge.cpp
@@ -8,15 +8,23 @@
 #include "./Longan_CANFD/src/mcp2518fd_can.hpp"
 #include "usb_can_app_i.hpp"
 
+#define WAIT_CDC()                                                                          \
+    while(true) {                                                                           \
+        furi_check(furi_mutex_acquire(usb_can->tx_mutex, FuriWaitForever) == FuriStatusOk); \
+        if(!cdc_busy) {                                                                     \
+            cdc_busy = true;                                                                \
+            furi_check(furi_mutex_release(usb_can->tx_mutex) == FuriStatusOk);              \
+            break;                                                                          \
+        }                                                                                   \
+        furi_check(furi_mutex_release(usb_can->tx_mutex) == FuriStatusOk);                  \
+    }
+
 const char serma_banner[] =
     "\r\n"
-    " ::::::::  :::::::::: :::::::::  ::::    ::::      :::          :::::::::: :::::::::   :::::::        ::::::::      :::     ::::    :::      :::::::::: :::::::::  \r\n"
-    ":+:    :+: :+:        :+:    :+: +:+:+: :+:+:+   :+: :+:        :+:        :+:    :+: :+:   :+:      :+:    :+:   :+: :+:   :+:+:   :+:      :+:        :+:    :+: \r\n"
-    "+:+        +:+        +:+    +:+ +:+ +:+:+ +:+  +:+   +:+       +:+        +:+    +:+ +:+  :+:+      +:+         +:+   +:+  :+:+:+  +:+      +:+        +:+    +:+ \r\n"
-    "+#++:++#++ +#++:++#   +#++:++#:  +#+  +:+  +#+ +#++:++#++:      :#::+::#   +#++:++#+  +#+ + +:+      +#+        +#++:++#++: +#+ +:+ +#+      :#::+::#   +#+    +:+ \r\n"
-    "       +#+ +#+        +#+    +#+ +#+       +#+ +#+     +#+      +#+        +#+        +#+#  +#+      +#+        +#+     +#+ +#+  +#+#+#      +#+        +#+    +#+ \r\n"
-    "#+#    #+# #+#        #+#    #+# #+#       #+# #+#     #+#      #+#        #+#        #+#   #+#      #+#    #+# #+#     #+# #+#   #+#+#      #+#        #+#    #+# \r\n"
-    " ########  ########## ###    ### ###       ### ###     ###      ###        ###         #######        ########  ###     ### ###    ####      ###        #########  \r\n"
+    "   ___________  __  ______     _______  ___    ________   _  __  _______ \r\n"
+    "  / __/ __/ _ \\/  |/  / _ |   / __/ _ \\/ _ \\  / ___/ _ | / |/ / / __/ _ \\ \r\n"
+    " _\\ \\/ _// , _/ /|_/ / __ |  / _// ___/ // / / /__/ __ |/    / / _// // /\r\n"
+    "/___/___/_/|_/_/  /_/_/ |_| /_/ /_/   \\___/  \\___/_/ |_/_/|_/ /_/ /____/ \r\n"
     "Serma Safety and Security (S3) - 2024 \r\n";
 
 #define usb_can_RX_BUF_SIZE (USB_CDC_PKT_LEN * 5)
@@ -69,7 +77,6 @@ static const CdcCallbacks cdc_cb = {
     vcp_on_cdc_control_line,
     vcp_on_line_config,
 };
-
 static bool can_if_initialized;
 const char invalid_length[] = "[err]: invalid command length (command %c) :length = %d\r\n";
 /* USB UART worker */
@@ -95,17 +102,18 @@ static void usb_can_vcp_deinit(UsbCanBridge* usb_can, uint8_t vcp_ch) {
         furi_record_close(RECORD_CLI);
     }
 }
-/*static void usb_can_print_banner(UsbCanBridge* usb_can) {
+static volatile bool cdc_busy;
+static void usb_can_print_banner(UsbCanBridge* usb_can) {
     uint32_t i;
+    (void)usb_can;
 
     for(i = 0; i < sizeof(serma_banner) - CDC_DATA_SZ; i += CDC_DATA_SZ) {
-        while(furi_mutex_acquire(usb_can->tx_mutex, FuriWaitForever) != FuriStatusOk)
-            ;
+        WAIT_CDC();
         furi_hal_cdc_send(usb_can->cfg.vcp_ch, (uint8_t*)&serma_banner[i], CDC_DATA_SZ);
-        furi_check(furi_mutex_release(usb_can->tx_mutex) == FuriStatusOk);
     }
+    WAIT_CDC();
     furi_hal_cdc_send(usb_can->cfg.vcp_ch, (uint8_t*)&serma_banner[i], sizeof(serma_banner) - i);
-}*/
+}
 static int32_t usb_can_worker(void* context) {
     UsbCanBridge* usb_can = (UsbCanBridge*)context;
     uint32_t events = 0;
@@ -139,19 +147,18 @@ static int32_t usb_can_worker(void* context) {
     usb_can->tx_thread = furi_thread_alloc_ex("UsbCanTxWorker", 2048, usb_can_tx_thread, usb_can);
 
     usb_can_vcp_init(usb_can, usb_can->cfg.vcp_ch);
-    //usb_can_print_banner(usb_can);
-    if(furi_mutex_acquire(usb_can->tx_mutex, FuriWaitForever) == FuriStatusOk) {
-        if(usb_can->state != UsbCanLoopBackTestState) {
-            const char hello_bridge_usb[] = "USB BRIDGE STARTED\r\n";
-            furi_hal_cdc_send(
-                usb_can->cfg.vcp_ch, (uint8_t*)hello_bridge_usb, sizeof(hello_bridge_usb));
-        } else {
-            const char hello_test_usb[] = "USB LOOPBACK TEST START\r\n";
-            furi_hal_cdc_send(
-                usb_can->cfg.vcp_ch, (uint8_t*)hello_test_usb, sizeof(hello_test_usb));
-        }
-        furi_check(furi_mutex_release(usb_can->tx_mutex) == FuriStatusOk);
+    usb_can_print_banner(usb_can);
+
+    WAIT_CDC();
+    if(usb_can->state != UsbCanLoopBackTestState) {
+        const char hello_bridge_usb[] = "USB BRIDGE STARTED\r\n";
+        furi_hal_cdc_send(
+            usb_can->cfg.vcp_ch, (uint8_t*)hello_bridge_usb, sizeof(hello_bridge_usb));
+    } else {
+        const char hello_test_usb[] = "USB LOOPBACK TEST START\r\n";
+        furi_hal_cdc_send(usb_can->cfg.vcp_ch, (uint8_t*)hello_test_usb, sizeof(hello_test_usb));
     }
+
     furi_thread_start(usb_can->tx_thread);
 
     while(1) {
@@ -181,11 +188,8 @@ static int32_t usb_can_worker(void* context) {
                 printSize += snprintf(&tmp[printSize], 3, "%02lX", tmp_data);
             }
             printSize += snprintf(&tmp[printSize], 3, "\r\n");
-            if(furi_mutex_acquire(usb_can->tx_mutex, 500) == FuriStatusOk) {
-                furi_hal_cdc_send(usb_can->cfg.vcp_ch, (uint8_t*)tmp, printSize);
-                furi_check(furi_mutex_release(usb_can->tx_mutex) == FuriStatusOk);
-            }
-
+            WAIT_CDC();
+            furi_hal_cdc_send(usb_can->cfg.vcp_ch, (uint8_t*)tmp, printSize);
             usb_can->st.rx_cnt += len;
         }
 
@@ -232,12 +236,12 @@ static int32_t usb_can_tx_thread(void* context) {
         furi_check(!(events & FuriFlagError));
         if(events & WorkerEvtTxStop) break;
         if(events & WorkerEvtCdcRx) {
-            furi_check(furi_mutex_acquire(usb_can->usb_mutex, 500) == FuriStatusOk);
+            WAIT_CDC();
             size_t len = furi_hal_cdc_receive(usb_can->cfg.vcp_ch, data, USB_CDC_PKT_LEN);
-            furi_check(furi_mutex_release(usb_can->usb_mutex) == FuriStatusOk);
+            cdc_busy = false;
             if(usb_can->state == UsbCanLoopBackTestState) {
-                furi_assert(furi_mutex_acquire(usb_can->tx_mutex, 500) == FuriStatusOk);
                 usb_can->st.tx_cnt += len;
+                WAIT_CDC();
                 furi_hal_cdc_send(usb_can->cfg.vcp_ch, data, len);
 
             } else {
@@ -408,17 +412,16 @@ static void usb_can_bridge_error(UsbCanBridge* usb_can, const char* err_msg_form
     char out_msg[64];
     va_list args;
     va_start(args, err_msg_format);
-    furi_assert(furi_mutex_acquire(usb_can->tx_mutex, 500) == FuriStatusOk);
     printSize = vsprintf(out_msg, err_msg_format, args);
+    WAIT_CDC();
     furi_hal_cdc_send(usb_can->cfg.vcp_ch, (uint8_t*)out_msg, printSize);
-    furi_assert(furi_mutex_release(usb_can->tx_mutex) == FuriStatusOk);
 }
 
 /* VCP callbacks */
 
 static void vcp_on_cdc_tx_complete(void* context) {
     (void)context;
-    //furi_assert(furi_mutex_release(usb_can->tx_mutex) == FuriStatusOk);
+    cdc_busy = false;
 }
 
 static void vcp_on_cdc_rx(void* context) {
@@ -454,7 +457,7 @@ UsbCanBridge* usb_can_enable(UsbCanApp* app, usbCanState state) {
 }
 
 void usb_can_disable(UsbCanBridge* usb_can) {
-    furi_assert(usb_can);
+    furi_check(usb_can);
     furi_thread_flags_set(furi_thread_get_id(usb_can->thread), WorkerEvtStop);
     furi_thread_join(usb_can->thread);
     furi_thread_free(usb_can->thread);
@@ -462,8 +465,8 @@ void usb_can_disable(UsbCanBridge* usb_can) {
 }
 
 void usb_can_set_config(UsbCanBridge* usb_can, UsbCanConfig* cfg) {
-    furi_assert(usb_can);
-    furi_assert(cfg);
+    furi_check(usb_can);
+    furi_check(cfg);
     usb_can->cfg_lock = api_lock_alloc_locked();
     memcpy(&(usb_can->cfg_new), cfg, sizeof(UsbCanConfig));
     furi_thread_flags_set(furi_thread_get_id(usb_can->thread), WorkerEvtCfgChange);
@@ -471,13 +474,13 @@ void usb_can_set_config(UsbCanBridge* usb_can, UsbCanConfig* cfg) {
 }
 
 void usb_can_get_config(UsbCanBridge* usb_can, UsbCanConfig* cfg) {
-    furi_assert(usb_can);
-    furi_assert(cfg);
+    furi_check(usb_can);
+    furi_check(cfg);
     memcpy(cfg, &(usb_can->cfg_new), sizeof(UsbCanConfig));
 }
 
 void usb_can_get_state(UsbCanBridge* usb_can, UsbCanStatus* st) {
-    furi_assert(usb_can);
-    furi_assert(st);
+    furi_check(usb_can);
+    furi_check(st);
     memcpy(st, &(usb_can->st), sizeof(UsbCanStatus));
 }
