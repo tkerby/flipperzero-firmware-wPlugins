1403abe35ea7bdf5c3d8c5325457d66e829f6698
RebornedBrain <RebornedBrain@gmail.com>
2024-08-27 14:40:28 +0300

    Screen instance is now destroyed when data saved

--- applications_user/nfc_eink/scenes/scene_save_success.c
+++ applications_user/nfc_eink/scenes/scene_save_success.c
@@ -35,7 +35,7 @@ bool nfc_eink_scene_save_success_on_event(void* context, SceneManagerEvent event
 
 void nfc_eink_scene_save_success_on_exit(void* context) {
     NfcEinkApp* nfc = context;
-
+    nfc_eink_screen_free(nfc->screen);
     // Clear view
     popup_reset(nfc->popup);
 }
