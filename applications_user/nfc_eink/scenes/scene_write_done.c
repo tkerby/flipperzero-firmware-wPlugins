5c3324d67ea7cd6bf322f178e6b6453e3bb592d9
RebornedBrain <RebornedBrain@gmail.com>
2024-09-05 23:35:49 +0300

    Moving back after write opeartion adjusted

--- applications_user/nfc_eink/scenes/scene_write_done.c
+++ applications_user/nfc_eink/scenes/scene_write_done.c
@@ -26,15 +26,9 @@ bool nfc_eink_scene_write_done_on_event(void* context, SceneManagerEvent event) 
 
     if(event.type == SceneManagerEventTypeCustom) {
         if(event.event == NfcEinkAppCustomEventTimerExpired) {
+            ///TODO: Move back to screen menu instead of saved and file select
             consumed = scene_manager_search_and_switch_to_previous_scene(
-                instance->scene_manager, NfcEinkAppSceneFileSelect);
-            /*  if(nfc_load_file(nfc, nfc->file_path, false)) {
-                consumed = scene_manager_search_and_switch_to_previous_scene(
-                    nfc->scene_manager, NfcSceneSavedMenu);
-            } else {
-                consumed = scene_manager_search_and_switch_to_previous_scene(
-                    nfc->scene_manager, NfcSceneFileSelect);
-            } */
+                instance->scene_manager, NfcEinkAppSceneSavedMenu);
         }
     }
     return consumed;
