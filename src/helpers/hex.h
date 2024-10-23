0b190b912d254eec911068312103af7e98036424
object-Object <object@objectobject.ca>
2024-10-23 16:59:05 -0400

    Implement access code decryption

--- src/helpers/hex.h
+++ src/helpers/hex.h
@@ -5,14 +5,22 @@
 // https://stackoverflow.com/a/57723618
 bool to_hex(char* dest, size_t dest_len, const uint8_t* values, size_t val_len) {
     static const char hex_table[] = "0123456789ABCDEF";
-    if (dest_len < (val_len * 2 + 1)) /* check that dest is large enough */
+    if (dest_len < (val_len * 3)) /* check that dest is large enough */
         return false;
     while (val_len--) {
         /* shift down the top nibble and pick a char from the hex_table */
         *dest++ = hex_table[*values >> 4];
         /* extract the bottom nibble and pick a char from the hex_table */
         *dest++ = hex_table[*values++ & 0xF];
+        if (val_len) *dest++ = ':';
     }
     *dest = 0;
     return true;
+}
+
+char* to_hex_alloc(const uint8_t* values, size_t val_len) {
+    size_t dest_len = val_len * 3;
+    char* dest = malloc(dest_len);
+    to_hex(dest, dest_len, values, val_len);
+    return dest;
 }
--- src/scenes/flip_aic_scene_read.c
+++ src/scenes/flip_aic_scene_read.c
@@ -1,9 +1,7 @@
+#include "../helpers/aic.h"
 #include "../helpers/hex.h"
 #include "../flip_aic.h"
 #include "flip_aic_scene.h"
-
-#include <lib/nfc/protocols/mf_desfire/mf_desfire.h>
-#include <lib/nfc/protocols/mf_desfire/mf_desfire_poller.h>
 
 #include <lib/nfc/protocols/felica/felica.h>
 #include <lib/nfc/protocols/felica/felica_poller.h>
@@ -16,9 +14,27 @@ enum PollerResult {
 static NfcCommand flip_aic_scene_read_poller_callback(NfcGenericEvent event, void* context) {
     furi_assert(context);
     FlipAIC* app = context;
-    MfDesfirePollerEvent* poller_event = event.event_data;
-    view_dispatcher_send_custom_event(app->view_dispatcher, poller_event->type);
-    return NfcCommandStop;
+
+    FelicaPollerEvent* poller_event = event.event_data;
+    FURI_LOG_I(TAG, "Poller event: %d", poller_event->type);
+
+    switch (poller_event->type) {
+    case FelicaPollerEventTypeReady:
+    case FelicaPollerEventTypeIncomplete:
+        view_dispatcher_send_custom_event(app->view_dispatcher, PollerResultSuccess);
+        return NfcCommandStop;
+
+    case FelicaPollerEventTypeError:
+        FURI_LOG_I(TAG, "Error: %d", poller_event->data->error);
+        view_dispatcher_send_custom_event(app->view_dispatcher, PollerResultFailed);
+        return NfcCommandStop;
+
+    case FelicaPollerEventTypeRequestAuthContext:
+        poller_event->data->auth_context->skip_auth = true;
+        return NfcCommandContinue;
+    }
+
+    return NfcCommandContinue; // why is this needed????
 }
 
 void flip_aic_scene_read_on_enter(void* context) {
@@ -42,28 +58,31 @@ bool flip_aic_scene_read_on_event(void* context, SceneManagerEvent event) {
 
     if (event.type == SceneManagerEventTypeCustom) {
         switch (event.event) {
-        case MfDesfirePollerEventTypeReadSuccess:;
-            NfcDevice* device = nfc_device_alloc();
-            nfc_device_set_data(device, app->nfc_protocol, nfc_poller_get_data(app->nfc_poller));
+        case PollerResultSuccess:
+            const FelicaData* data = nfc_poller_get_data(app->nfc_poller);
 
-            size_t uid_len;
-            const uint8_t* uid = nfc_device_get_uid(device, &uid_len);
+            char* idm = to_hex_alloc(data->idm.data, sizeof(data->idm.data));
+            FURI_LOG_I(TAG, "IDm: %s", idm);
+            free(idm);
 
-            size_t uid_s_len = uid_len * 2 + 1;
-            char* uid_s = malloc(uid_s_len);
-            to_hex(uid_s, uid_s_len, uid, uid_len);
+            char* pmm = to_hex_alloc(data->pmm.data, sizeof(data->pmm.data));
+            FURI_LOG_I(TAG, "PMm: %s", pmm);
+            free(pmm);
 
-            FURI_LOG_I(TAG, "Name: %s", nfc_device_get_name(device, NfcDeviceNameTypeFull));
-            FURI_LOG_I(TAG, "UID:  %s", uid_s);
+            uint8_t spad0[16];
+            aic_access_code_decrypt(data->data.fs.spad[0].data, spad0);
 
-            free(uid_s);
-            nfc_device_free(device);
+            char access_code[3 * 10];
+            to_hex(access_code, sizeof(access_code), spad0 + 6, 10);
+            FURI_LOG_I(TAG, "Access code: %s", access_code);
 
-        case MfDesfirePollerEventTypeReadFailed:
-            break;
+            scene_manager_next_scene(app->scene_manager, FlipAICSceneMenu);
+            return true;
+
+        case PollerResultFailed:
+            scene_manager_next_scene(app->scene_manager, FlipAICSceneMenu);
+            return true;
         }
-        scene_manager_next_scene(app->scene_manager, FlipAICSceneMenu);
-        return true;
     }
 
     return false;
