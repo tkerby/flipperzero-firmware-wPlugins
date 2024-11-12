154092a69c61572bc2365847c3180cfe4b730109
RogueMaster <RogueMaster@users.noreply.github.com>
2024-11-11 19:46:18 -0500

    Latest RM1111-1340-0.420.0-b2dac97 on PATREON & GitHub - ADD ENCODER READER

--- applications/external/enc_reader/enc_reader.c
+++ applications/external/enc_reader/enc_reader.c
@@ -13,137 +13,161 @@
 #include <notification/notification.h>
 
 static void enc_reader_app_input_callback(InputEvent* input_event, void* context) {
-	furi_assert(context);
+    furi_assert(context);
 
-	FuriMessageQueue* event_queue = context;
-	furi_message_queue_put(event_queue, input_event, FuriWaitForever);
+    FuriMessageQueue* event_queue = context;
+    furi_message_queue_put(event_queue, input_event, FuriWaitForever);
 }
 
 static void enc_reader_app_draw_callback(Canvas* canvas, void* context) {
-	furi_assert(context);
-	EncApp* app = context;
-	
-	char abs_coord[] = "00000000 ";
-	char org_coord[] = "00000000 ";
-	char rel_coord[] = "00000000 ";
+    furi_assert(context);
+    EncApp* app = context;
 
-	static const uint8_t header_height		= 12;
-	static const uint8_t vertical_gap		= 2;
-	static const uint8_t vertical_offset	= 16;
+    char abs_coord[] = "00000000 ";
+    char org_coord[] = "00000000 ";
+    char rel_coord[] = "00000000 ";
 
-	snprintf(abs_coord, strlen(abs_coord), "%8d", (int)app->coordinates.abs);
-	snprintf(org_coord, strlen(org_coord), "%8d", (int)app->coordinates.org);
-	snprintf(rel_coord, strlen(rel_coord), "%8d", (int)app->coordinates.rel);
+    static const uint8_t header_height = 12;
+    static const uint8_t vertical_gap = 2;
+    static const uint8_t vertical_offset = 16;
 
-	canvas_clear(canvas);
-	canvas_set_font(canvas, FontSecondary);
+    snprintf(abs_coord, strlen(abs_coord), "%8d", (int)app->coordinates.abs);
+    snprintf(org_coord, strlen(org_coord), "%8d", (int)app->coordinates.org);
+    snprintf(rel_coord, strlen(rel_coord), "%8d", (int)app->coordinates.rel);
 
-	elements_multiline_text_aligned(canvas, 4, vertical_gap, AlignLeft, AlignTop, app->Vbus_state ? "Bus:    enable" : "Bus:   disable");
+    canvas_clear(canvas);
+    canvas_set_font(canvas, FontSecondary);
 
-	elements_frame(canvas, 0, header_height, canvas_width(canvas), canvas_height(canvas) - header_height);
+    elements_multiline_text_aligned(
+        canvas,
+        4,
+        vertical_gap,
+        AlignLeft,
+        AlignTop,
+        app->Vbus_state ? "Bus:    enable" : "Bus:   disable");
 
-	elements_multiline_text_aligned(canvas, 4, header_height + vertical_gap,						AlignLeft, AlignTop, "Abs:");
-	elements_multiline_text_aligned(canvas, 4, header_height + vertical_gap + vertical_offset,		AlignLeft, AlignTop, "Org:");
-	elements_multiline_text_aligned(canvas, 4, header_height + vertical_gap + vertical_offset * 2,	AlignLeft, AlignTop, "Rel:");
-	
-	canvas_set_font(canvas, FontBigNumbers);
-	elements_multiline_text_aligned(canvas, 30, header_height + vertical_gap,						AlignLeft, AlignTop, (const char*)abs_coord);
-	elements_multiline_text_aligned(canvas, 30, header_height + vertical_gap + vertical_offset,		AlignLeft, AlignTop, (const char*)org_coord);
-	elements_multiline_text_aligned(canvas, 30, header_height + vertical_gap + vertical_offset * 2,	AlignLeft, AlignTop, (const char*)rel_coord);
+    elements_frame(
+        canvas, 0, header_height, canvas_width(canvas), canvas_height(canvas) - header_height);
+
+    elements_multiline_text_aligned(
+        canvas, 4, header_height + vertical_gap, AlignLeft, AlignTop, "Abs:");
+    elements_multiline_text_aligned(
+        canvas, 4, header_height + vertical_gap + vertical_offset, AlignLeft, AlignTop, "Org:");
+    elements_multiline_text_aligned(
+        canvas, 4, header_height + vertical_gap + vertical_offset * 2, AlignLeft, AlignTop, "Rel:");
+
+    canvas_set_font(canvas, FontBigNumbers);
+    elements_multiline_text_aligned(
+        canvas, 30, header_height + vertical_gap, AlignLeft, AlignTop, (const char*)abs_coord);
+    elements_multiline_text_aligned(
+        canvas,
+        30,
+        header_height + vertical_gap + vertical_offset,
+        AlignLeft,
+        AlignTop,
+        (const char*)org_coord);
+    elements_multiline_text_aligned(
+        canvas,
+        30,
+        header_height + vertical_gap + vertical_offset * 2,
+        AlignLeft,
+        AlignTop,
+        (const char*)rel_coord);
 }
 
 static void enc_reader_app_interrupt_callback(void* context) {
-	furi_assert(context);
-	EncApp* app = context;
+    furi_assert(context);
+    EncApp* app = context;
 
-	if (furi_hal_gpio_read(app->input_pin.b))	app->coordinates.abs++;
-	else										app->coordinates.abs--;
+    if(furi_hal_gpio_read(app->input_pin.b))
+        app->coordinates.abs++;
+    else
+        app->coordinates.abs--;
 }
 
 EncApp* enc_reader_app_alloc() {
-	EncApp* app = malloc(sizeof(EncApp));
+    EncApp* app = malloc(sizeof(EncApp));
 
-	app->gui			= furi_record_open(RECORD_GUI);
-	app->view_port		= view_port_alloc();
-	app->event_queue	= furi_message_queue_alloc(8, sizeof(InputEvent));
-	app->input_pin.a	= &gpio_ext_pa4;
-	app->input_pin.b	= &gpio_ext_pa7;
+    app->gui = furi_record_open(RECORD_GUI);
+    app->view_port = view_port_alloc();
+    app->event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
+    app->input_pin.a = &gpio_ext_pa4;
+    app->input_pin.b = &gpio_ext_pa7;
 
-	furi_hal_gpio_init(app->input_pin.a, GpioModeInterruptFall,	GpioPullUp, GpioSpeedVeryHigh);
-	furi_hal_gpio_init(app->input_pin.b, GpioModeInput,			GpioPullUp, GpioSpeedVeryHigh);
+    furi_hal_gpio_init(app->input_pin.a, GpioModeInterruptFall, GpioPullUp, GpioSpeedVeryHigh);
+    furi_hal_gpio_init(app->input_pin.b, GpioModeInput, GpioPullUp, GpioSpeedVeryHigh);
 
-	// Attach interrupt to pin A
-	furi_hal_gpio_add_int_callback(app->input_pin.a, enc_reader_app_interrupt_callback, app);
-	furi_hal_gpio_enable_int_callback(app->input_pin.a);
-	
-	gui_add_view_port(app->gui, app->view_port, GuiLayerFullscreen);
-	
-	view_port_input_callback_set(app->view_port, enc_reader_app_input_callback, app->event_queue);
-	view_port_draw_callback_set(app->view_port, enc_reader_app_draw_callback, app);
+    // Attach interrupt to pin A
+    furi_hal_gpio_add_int_callback(app->input_pin.a, enc_reader_app_interrupt_callback, app);
+    furi_hal_gpio_enable_int_callback(app->input_pin.a);
 
-	app->notifications = furi_record_open(RECORD_NOTIFICATION);
+    gui_add_view_port(app->gui, app->view_port, GuiLayerFullscreen);
 
-	return app;
+    view_port_input_callback_set(app->view_port, enc_reader_app_input_callback, app->event_queue);
+    view_port_draw_callback_set(app->view_port, enc_reader_app_draw_callback, app);
+
+    app->notifications = furi_record_open(RECORD_NOTIFICATION);
+
+    return app;
 }
 
 void enc_reader_app_free(EncApp* app) {
-	furi_assert(app);
+    furi_assert(app);
 
-	furi_record_close(RECORD_NOTIFICATION);
+    furi_record_close(RECORD_NOTIFICATION);
 
-	furi_hal_gpio_disable_int_callback(app->input_pin.a);
-	furi_hal_gpio_remove_int_callback(app->input_pin.a);
+    furi_hal_gpio_disable_int_callback(app->input_pin.a);
+    furi_hal_gpio_remove_int_callback(app->input_pin.a);
 
-	furi_message_queue_free(app->event_queue);
-	view_port_enabled_set(app->view_port, false);
-	gui_remove_view_port(app->gui, app->view_port);
-	view_port_free(app->view_port);
+    furi_message_queue_free(app->event_queue);
+    view_port_enabled_set(app->view_port, false);
+    gui_remove_view_port(app->gui, app->view_port);
+    view_port_free(app->view_port);
 
-	furi_record_close(RECORD_GUI);
+    furi_record_close(RECORD_GUI);
 }
 
-int32_t enc_reader_app(void *p) {
-	UNUSED(p);
-	EncApp* app = enc_reader_app_alloc();
+int32_t enc_reader_app(void* p) {
+    UNUSED(p);
+    EncApp* app = enc_reader_app_alloc();
 
-	InputEvent event;
+    InputEvent event;
 
-	bool running = true;
+    bool running = true;
 
-	while (running) {
+    while(running) {
+        app->coordinates.rel = app->coordinates.abs - app->coordinates.org;
 
-		app->coordinates.rel = app->coordinates.abs - app->coordinates.org;
+        if(furi_message_queue_get(app->event_queue, &event, 200) == FuriStatusOk) {
+            if(event.key == InputKeyBack) {
+                if(event.type == InputTypeLong) running = false;
+            } else if(event.key == InputKeyOk) {
+                if(event.type == InputTypeShort) {
+                    app->coordinates.org = app->coordinates.abs;
+                    notification_message(app->notifications, &button_led_sequence);
+                }
+            } else if(event.key == InputKeyDown) {
+                if(event.type == InputTypeShort) {
+                    app->coordinates.org = 0;
+                    app->coordinates.abs = 0;
+                    notification_message(app->notifications, &button_led_sequence);
+                }
+            } else if(event.key == InputKeyUp) {
+                if(event.type == InputTypeShort) {
+                    app->Vbus_state = !app->Vbus_state;
 
-		if (furi_message_queue_get(app->event_queue, &event, 200) == FuriStatusOk) {
-			if (event.key == InputKeyBack) {
-				if (event.type == InputTypeLong) running = false;
-			} else if (event.key == InputKeyOk) {
-				if (event.type == InputTypeShort) {
-					app->coordinates.org = app->coordinates.abs;
-					notification_message(app->notifications, &button_led_sequence);
-				}
-			} else if (event.key == InputKeyDown) {
-				if (event.type == InputTypeShort) {
-					app->coordinates.org = 0;
-					app->coordinates.abs = 0;
-					notification_message(app->notifications, &button_led_sequence);
-				}
-			} else if (event.key == InputKeyUp) {
-				if (event.type == InputTypeShort) {
-					app->Vbus_state = !app->Vbus_state;
-
-					if (app->Vbus_state)	{
-						furi_hal_power_enable_otg();
-						notification_message(app->notifications, &vOn_led_sequence);
-					} else {
-						furi_hal_power_disable_otg();
-						notification_message(app->notifications, &vOff_led_sequence);
-					}
-				}
-			}
-		}
-	}
-	furi_hal_power_disable_otg();
-	enc_reader_app_free(app);
-	return 0;
+                    if(app->Vbus_state) {
+                        furi_hal_power_enable_otg();
+                        notification_message(app->notifications, &vOn_led_sequence);
+                    } else {
+                        furi_hal_power_disable_otg();
+                        notification_message(app->notifications, &vOff_led_sequence);
+                    }
+                }
+            }
+        }
+    }
+    furi_hal_power_disable_otg();
+    enc_reader_app_free(app);
+    return 0;
 }
--- applications/external/enc_reader/enc_reader.h
+++ applications/external/enc_reader/enc_reader.h
@@ -7,10 +7,10 @@
 #include <notification/notification_messages.h>
 
 const NotificationSequence button_led_sequence = {
-	&message_blue_255,
-	&message_vibro_on,
-	&message_delay_50,
-	&message_vibro_off,
+    &message_blue_255,
+    &message_vibro_on,
+    &message_delay_50,
+    &message_vibro_off,
     &message_delay_500,
     NULL,
 };
@@ -18,43 +18,43 @@ const NotificationSequence button_led_sequence = {
 const NotificationSequence vOn_led_sequence = {
     &message_green_255,
     &message_vibro_on,
-	&message_delay_50,
-	&message_vibro_off,
+    &message_delay_50,
+    &message_vibro_off,
     &message_delay_100,
     &message_vibro_on,
-	&message_delay_50,
-	&message_vibro_off,
+    &message_delay_50,
+    &message_vibro_off,
     NULL,
 };
 
 const NotificationSequence vOff_led_sequence = {
     &message_red_255,
     &message_vibro_on,
-	&message_delay_250,
-	&message_vibro_off,
+    &message_delay_250,
+    &message_vibro_off,
     NULL,
 };
 
-typedef struct EncApp{
+typedef struct EncApp {
     Gui* gui;
     ViewPort* view_port;
-	FuriMessageQueue* event_queue;
-	NotificationApp* notifications;
+    FuriMessageQueue* event_queue;
+    NotificationApp* notifications;
 
-	struct {
-		const GpioPin*	a;
-		const GpioPin*	b;
-	} input_pin;
+    struct {
+        const GpioPin* a;
+        const GpioPin* b;
+    } input_pin;
 
-	struct {
-		const GpioPin*	pp;
-	} output_pin;
+    struct {
+        const GpioPin* pp;
+    } output_pin;
 
-	struct {
-		int32_t abs;
-		int32_t org;
-		int32_t rel;
-	} coordinates;
+    struct {
+        int32_t abs;
+        int32_t org;
+        int32_t rel;
+    } coordinates;
 
-	bool Vbus_state;
+    bool Vbus_state;
 } EncApp;
