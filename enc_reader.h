0f08d2d9906cae60b67988b7de3f9ab15afaa437
Engineegor <igorcia.ifl@gmail.com>
2024-11-15 13:18:30 +0500

    Added configuration screen
    commit bea9bd9214215033bc316e0ffda2b2c42d0a4ea0
    Author: Engineegor <igorcia.ifl@gmail.com>
    Date:   Fri Nov 15 13:17:31 2024 +0500
    
        Add configuration screen
    
    commit bc7dbb2df09faf17faae41a6bc998823c4eba477
    Author: Engineegor <igorcia.ifl@gmail.com>
    Date:   Wed Nov 13 22:55:34 2024 +0500
    
        Save point
    
    commit 4be6f6dde475b22383c340b0273865023de9c495
    Author: Engineegor <igorcia.ifl@gmail.com>
    Date:   Wed Nov 13 21:31:08 2024 +0500
    
        Save point
    
    commit 36b6438032cbbb26d63992edd31c32ca90ec89b4
    Author: Engineegor <igorcia.ifl@gmail.com>
    Date:   Wed Nov 13 19:44:18 2024 +0500
    
        Save point
    
    commit 2877723c490283fa4a3feef1d0ce7d0f76fb9f97
    Author: Engineegor <igorcia.ifl@gmail.com>
    Date:   Wed Nov 13 19:34:37 2024 +0500
    
        Save point
    
    commit e19746ceb9b038f2f8b623eb7d3c9590b8c8e0eb
    Author: Engineegor <igorcia.ifl@gmail.com>
    Date:   Wed Nov 13 14:34:55 2024 +0500
    
        Add view_dispatcher

--- enc_reader.h
+++ enc_reader.h
@@ -2,59 +2,126 @@
 
 #include <furi.h>
 #include <furi_hal.h>
+
 #include <gui/gui.h>
+#include <gui/view_dispatcher.h>
+#include <gui/modules/widget.h>
+#include <gui/modules/variable_item_list.h>
+
 #include <notification/notification.h>
 #include <notification/notification_messages.h>
 
 const NotificationSequence button_led_sequence = {
-	&message_blue_255,
-	&message_vibro_on,
-	&message_delay_50,
-	&message_vibro_off,
-    &message_delay_500,
+    &message_blue_255,
+    &message_vibro_on,
+    &message_delay_25,
+    &message_vibro_off,
+    &message_delay_250,
+    &message_blue_0,
     NULL,
 };
-
 const NotificationSequence vOn_led_sequence = {
     &message_green_255,
     &message_vibro_on,
-	&message_delay_50,
-	&message_vibro_off,
+    &message_delay_25,
+    &message_vibro_off,
     &message_delay_100,
     &message_vibro_on,
-	&message_delay_50,
-	&message_vibro_off,
+    &message_delay_25,
+    &message_vibro_off,
     NULL,
 };
-
 const NotificationSequence vOff_led_sequence = {
     &message_red_255,
     &message_vibro_on,
-	&message_delay_250,
-	&message_vibro_off,
+    &message_delay_100,
+    &message_delay_50,
+    &message_vibro_off,
     NULL,
 };
 
+// Enumeration of the view indexes.
+typedef enum {
+    ViewIndexSettings,
+    ViewIndexMain,
+    ViewIndexMAX,
+} ViewIndex;
+
+// Enumeration of submenu items.
+typedef enum {
+    ItemIndexStart,
+    ItemIndexResolution,
+    ItemIndexVbusState,
+    ItemIndexMAX,
+} ItemIndex;
+
+typedef enum {
+    ResolutionRaw,
+    Resolution50,
+    Resolution100,
+    Resolution200,
+    ResolutionMAX,
+} ResolutionIndex;
+
+typedef struct {
+    int32_t			divider;
+    char*			text;
+} resolution_t;
+
+const resolution_t resolution_list[] = {
+    {1,		"Raw"},
+    {50,	"50pul"},
+    {100,	"100pul"},
+    {200,	"200pul"},
+};
+
+typedef enum {
+    VbusOFF,
+    VbusON,
+    VbusStatesNum,
+} VbusState;
+
+const char* const vbus_state_list[] = {
+    "OFF",
+    "ON",
+};
+
+typedef enum {
+    IndexWidgetAbs,
+    IndexWidgetRel,
+    IndexWidgetMAX,
+} WidgetIndex;
+
+typedef void (*MainViewOkCallback)(InputType type, void* context);
+
+
 typedef struct EncApp{
     Gui* gui;
-    ViewPort* view_port;
-	FuriMessageQueue* event_queue;
-	NotificationApp* notifications;
+    NotificationApp* notifications;
 
-	struct {
-		const GpioPin*	a;
-		const GpioPin*	b;
-	} input_pin;
+    ViewDispatcher* view_dispatcher;
 
-	struct {
-		const GpioPin*	pp;
-	} output_pin;
+    //View* main_view;
 
-	struct {
-		int32_t abs;
-		int32_t org;
-		int32_t rel;
-	} coordinates;
+    Widget* widget;
+    WidgetElement* widget_element[IndexWidgetMAX];
+    
+    VariableItemList* var_item_list;
+    VariableItem* var_item[ItemIndexMAX];
 
-	bool Vbus_state;
+    struct {
+        const GpioPin*	a;
+        const GpioPin*	b;
+    } input_pin;
+
+    struct {
+        int32_t abs;
+        int32_t org;
+        int32_t rel;
+    } coordinates;
+
+    VbusState 		Vbus_state;
+    ResolutionIndex resolution;
+    ViewIndex		current_view;
+
 } EncApp;
