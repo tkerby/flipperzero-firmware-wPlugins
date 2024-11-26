531e150403e1ad8633d1d962b0ed3c2828c7d3db
Luu <112649910+luu176@users.noreply.github.com>
2024-11-26 13:04:43 +0100

    Update metroflip_scene_start.c

--- scenes/metroflip_scene_start.c
+++ scenes/metroflip_scene_start.c
@@ -27,6 +27,9 @@ void metroflip_scene_start_on_enter(void* context) {
         MetroflipSceneMetromoney,
         metroflip_scene_start_submenu_callback,
         app);
+
+    submenu_add_item(
+        submenu, "Bip!", MetroflipSceneBip, metroflip_scene_start_submenu_callback, app);
     submenu_set_selected_item(
         submenu, scene_manager_get_scene_state(app->scene_manager, MetroflipSceneStart));
 
