1172cab0ddeb1aebfce2246aeb0cb22c34a26a32
RogueMaster <RogueMaster@users.noreply.github.com>
2024-12-08 14:49:23 -0500

    Latest RM1204-1241-0.420.0-7add7d3 on PATREON - UPD ESPFLASHER

--- applications/external/esp_flasher/scenes/esp_flasher_scene_quick.c
+++ applications/external/esp_flasher/scenes/esp_flasher_scene_quick.c
@@ -196,7 +196,7 @@ bool esp_flasher_scene_quick_on_event(void* context, SceneManagerEvent event) {
             boot = APP_DATA_PATH("assets/marauder/WifidevS2/esp32_marauder.ino.bootloader.bin");
             part = APP_DATA_PATH("assets/marauder/esp32_marauder.ino.partitions.bin");
             app0 = APP_DATA_PATH("assets/marauder/boot_app0.bin");
-            firm = APP_DATA_PATH("assets/marauder/WifidevS2/marauder_v0_13_10_flipper.bin");
+            firm = APP_DATA_PATH("assets/marauder/WifidevS2/marauder_v1_1_0_flipper.bin");
             break;
 
         case QuickS2Boot_Flipperhttp:
@@ -224,7 +224,7 @@ bool esp_flasher_scene_quick_on_event(void* context, SceneManagerEvent event) {
             boot = APP_DATA_PATH("assets/marauder/DevproWroom/esp32_marauder.ino.bootloader.bin");
             part = APP_DATA_PATH("assets/marauder/esp32_marauder.ino.partitions.bin");
             app0 = APP_DATA_PATH("assets/marauder/boot_app0.bin");
-            firm = APP_DATA_PATH("assets/marauder/DevproWroom/marauder_v0_13_10_devboardpro.bin");
+            firm = APP_DATA_PATH("assets/marauder/DevproWroom/marauder_v1_1_0_devboardpro.bin");
             break;
 
         case QuickWROOMBoot_Wardriver:
@@ -242,7 +242,7 @@ bool esp_flasher_scene_quick_on_event(void* context, SceneManagerEvent event) {
             boot = APP_DATA_PATH("assets/marauder/s3/esp32_marauder.ino.bootloader.bin");
             part = APP_DATA_PATH("assets/marauder/esp32_marauder.ino.partitions.bin");
             app0 = APP_DATA_PATH("assets/marauder/boot_app0.bin");
-            firm = APP_DATA_PATH("assets/marauder/s3/marauder_v0_13_10_multiboardS3.bin");
+            firm = APP_DATA_PATH("assets/marauder/s3/marauder_v1_1_0_multiboardS3.bin");
             break;
 
         case QuickS3Boot_Wardriver:
