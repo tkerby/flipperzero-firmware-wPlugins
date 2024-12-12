e264ffa330458386fb13ca29fcdbe767522dcffe
jblanked <82678820+jblanked@users.noreply.github.com>
2024-12-12 16:58:36 -0500

    add Wall from Air Labyrinth

--- game.h
+++ game.h
@@ -8,6 +8,21 @@
 #define WORLD_WIDTH 384
 #define WORLD_HEIGHT 192
 
+// from https://github.com/jamisonderek/flipper-zero-tutorials/blob/main/vgm/apps/air_labyrinth/walls.h
+typedef struct
+{
+    bool horizontal;
+    int x;
+    int y;
+    int length;
+} Wall;
+
+#define WALL(h, y, x, l)   \
+    (Wall)                 \
+    {                      \
+        h, x * 2, y * 2, l \
+    }
+
 typedef struct
 {
     uint32_t score;
