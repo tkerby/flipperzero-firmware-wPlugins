342dc614a338daab43c81a11c1326b0f47c128d7
jblanked <82678820+jblanked@users.noreply.github.com>
2024-12-17 11:47:49 -0500

    Merge

--- draw/world.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <draw/draw.h>
-#include <game.h>
-#include <flip_world.h>
-void draw_bounds(Canvas *canvas);
-void draw_example_world(Level *level);
-void draw_tree_world(Level *level);
-bool draw_json_world(Level *level, FuriString *json_data);
