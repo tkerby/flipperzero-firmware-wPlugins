7387608a5766fdb0fbdd601bed9800c9d305074b
Ivan Barsukov <i.barsukov@protonmail.com>
2024-12-26 11:03:33 +0700

    Add game sources

---
+++ src/game.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <stdint.h>
+
+#include "engine/engine.h" // IWYU pragma: keep
+
+#define GAME_NAME "Quadrastic"
+
+typedef enum
+{
+    DifficultyEasy,
+    DifficultyNormal,
+    DifficultyHard,
+    DifficultyInsane,
+    DifficultyCount,
+} Difficulty;
+
+typedef enum
+{
+    StateOff,
+    StateOn,
+    StateCount,
+} State;
+
+typedef enum
+{
+    GameEventStopAnimation,
+} GameEvent;
+
+typedef struct
+{
+    Level* menu;
+    Level* game;
+    Level* game_over;
+    Level* settings;
+    Level* about;
+} Levels;
+
+typedef struct NotificationApp NotificationApp;
+
+typedef struct
+{
+    NotificationApp* notification;
+
+    Levels levels;
+    Difficulty difficulty;
+
+    uint32_t best_score;
+    uint32_t score;
+
+    State sound;
+    State vibro;
+    State led;
+
+} GameContext;
