3ef60379ed9d9806f3bc35363e876dc2617bd1d5
Ivan Barsukov <i.barsukov@protonmail.com>
2024-12-26 11:08:54 +0700

    Add view modules bridge for game engine

---
+++ src/gui_bridge/input_converter.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <stddef.h>
+
+#include <input/input.h>
+
+#include "../engine/game_engine.h"
+
+typedef struct InputConverter InputConverter;
+
+InputConverter*
+input_converter_alloc(void);
+
+void
+input_converter_free(InputConverter* input_converter);
+
+void
+input_converter_process_state(InputConverter* input_converter,
+                              InputState* input_state);
+
+FuriStatus
+input_converter_get_event(InputConverter* input_converter, InputEvent* event);
