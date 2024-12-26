29a6afbaeec6fd68866195ff33a2a182ca430648
Ivan Barsukov <i.barsukov@protonmail.com>
2024-12-26 11:05:29 +0700

    Add game notifications

---
+++ src/game_notifications.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <notification/notification.h>
+
+#include "game.h"
+
+extern const NotificationSequence sequence_earn_point;
+
+void
+game_notify(GameContext* game_context, const NotificationSequence* sequence);
