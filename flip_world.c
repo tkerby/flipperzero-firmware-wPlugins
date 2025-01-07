da81df626f2b189a53a49d535c967ae1495357db
jblanked <82678820+jblanked@users.noreply.github.com>
2025-01-07 18:36:03 -0500

    bump version, simplify is_enough_heap

--- flip_world.c
+++ flip_world.c
@@ -6,12 +6,4 @@ char *yes_or_no_choices[] = {"No", "Yes"};
 int game_screen_always_on_index = 1;
 int game_sound_on_index = 0;
 int game_vibration_on_index = 0;
-bool is_enough_heap(size_t heap_size)
-{
-    size_t free_heap = memmgr_get_free_heap();
-
-    FURI_LOG_I(TAG, "Free heap: %d", free_heap);
-    FURI_LOG_I(TAG, "Total heap: %d", memmgr_get_total_heap());
-
-    return free_heap > (heap_size + 1024); // 1KB buffer
-}
+bool is_enough_heap(size_t heap_size) { return memmgr_get_free_heap() > (heap_size + 1024); } // 1KB buffer
