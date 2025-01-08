9e22eb8d0ead3f9267793779532b42d6418412be
Cupprum <branisa.samuel@icloud.com>
2025-01-08 21:55:42 +0100

    feat: change from submenu to dialog

--- blinker.c
+++ blinker.c
@@ -2,12 +2,12 @@
 #include <furi_hal_light.h>
 #include <gui/gui.h>
 #include <gui/modules/number_input.h>
-#include <gui/modules/submenu.h>
+#include <gui/modules/dialog_ex.h>
 #include <gui/modules/widget.h>
 #include <gui/view_dispatcher.h>
 
 typedef enum {
-    BlinkerViewSubmenu,
+    BlinkerViewDialog,
     BlinkerViewWidget,
     BlinkerViewNumber,
 } BlinkerView;
@@ -16,7 +16,7 @@ typedef struct {
     Gui* gui;
     BlinkerView current_view;
     ViewDispatcher* view_dispatcher;
-    Submenu* submenu;
+    DialogEx* dialog;
     Widget* widget;
     NumberInput* number_input;
     FuriTimer* timer;
@@ -57,50 +57,27 @@ static void timer_callback(void* context) {
     }
 }
 
-static void start_blink_callback(void* context, uint32_t index) {
-    BlinkerApp* app = context;
-    UNUSED(index);
-    
-    widget_reset(app->widget);
-    widget_add_string_element(app->widget, 64, 32, AlignCenter, AlignCenter, FontPrimary, "Blinking");
-    
-    app->current_view = BlinkerViewWidget;
-    view_dispatcher_switch_to_view(app->view_dispatcher, BlinkerViewWidget);
-    
-    // Initialize blinking parameters
-    app->start_timestamp = furi_get_tick();
-    app->current_interval = 50; // NOTE: This value replicates min_sleep from timer_callback
-    furi_timer_start(app->timer, app->current_interval);
+
+static void update_main_view(BlinkerApp* app) {
+    dialog_ex_set_header(app->dialog, "Blinker", 64, 20, AlignCenter, AlignCenter);
+    dialog_ex_set_center_button_text(app->dialog, "Start");
+    dialog_ex_set_right_button_text(app->dialog, "Dur.");
+    char duration_text[32];
+    snprintf(duration_text, sizeof(duration_text), "Duration: %lds", app->duration);
+    dialog_ex_set_text(app->dialog, duration_text, 64, 32, AlignCenter, AlignCenter);
 }
+
 
 static void duration_callback(void* context, int32_t value) {
     BlinkerApp* app = context;
 
     app->duration = value;
-    app->current_view = BlinkerViewSubmenu;
-
-    view_dispatcher_switch_to_view(app->view_dispatcher, BlinkerViewSubmenu);
-}
-
-static void set_duration_callback(void* context, uint32_t index) {
-    BlinkerApp* app = context;
-    UNUSED(index);
+    app->current_view = BlinkerViewDialog;
     
-    app->current_view = BlinkerViewNumber;
-
-    NumberInput* number_input = app->number_input;
-
-    number_input_set_header_text(number_input, "Blink duration (sec)");
-    number_input_set_result_callback(
-        number_input,      // Number input module
-        duration_callback, // Function to call when value is set
-        context,
-        app->duration,     // Value which is being changed
-        5,                 // Min value
-        120                // Max value
-    );
+    // Update the main view to show new duration
+    update_main_view(app);
     
-    view_dispatcher_switch_to_view(app->view_dispatcher, BlinkerViewNumber);
+    view_dispatcher_switch_to_view(app->view_dispatcher, BlinkerViewDialog);
 }
 
 static bool back_button(void* context) {
@@ -109,20 +86,56 @@ static bool back_button(void* context) {
     if(app->current_view == BlinkerViewWidget) {
         // If in widget view, stop timer and LED, return to menu
         furi_timer_stop(app->timer);
-        // TODO: look at what values do i want the LED to be configured to
         furi_hal_light_set(LightRed, 0x00);
-        
-        app->current_view = BlinkerViewSubmenu;
-        view_dispatcher_switch_to_view(app->view_dispatcher, BlinkerViewSubmenu);
-        
-        return true;  // Exit to main menu
+        app->current_view = BlinkerViewDialog;
+        view_dispatcher_switch_to_view(app->view_dispatcher, BlinkerViewDialog);
+        return true;
     } else if(app->current_view == BlinkerViewNumber) {
-        app->current_view = BlinkerViewSubmenu;
-        view_dispatcher_switch_to_view(app->view_dispatcher, BlinkerViewSubmenu);
+        app->current_view = BlinkerViewDialog;
+        view_dispatcher_switch_to_view(app->view_dispatcher, BlinkerViewDialog);
+        return true;
+    } else if(app->current_view == BlinkerViewDialog) {
+        // We're in the main dialog view, allow exit
+        view_dispatcher_stop(app->view_dispatcher);
         return true;
     }
     
-    return false;  // Exit the app
+    return false;
+}
+
+static void dialog_callback(DialogExResult result, void* context) {
+    BlinkerApp* app = context;
+    
+    switch(result) {
+    case DialogExResultCenter:
+        // Start blinking (middle button)
+        widget_reset(app->widget);
+        widget_add_string_element(app->widget, 64, 32, AlignCenter, AlignCenter, FontPrimary, "Blinking");
+        app->current_view = BlinkerViewWidget;
+        view_dispatcher_switch_to_view(app->view_dispatcher, BlinkerViewWidget);
+        app->start_timestamp = furi_get_tick();
+        app->current_interval = 50;
+        furi_timer_start(app->timer, app->current_interval);
+        break;
+    case DialogExResultRight:
+        // Set duration (right button)
+        app->current_view = BlinkerViewNumber;
+
+        NumberInput* number_input = app->number_input;
+
+        number_input_set_header_text(number_input, "Blink duration (sec)");
+        number_input_set_result_callback(
+            number_input,
+            duration_callback,
+            app,           // Context
+            app->duration, // Current value
+            5,            // Min value
+            120);         // Max value
+        view_dispatcher_switch_to_view(app->view_dispatcher, BlinkerViewNumber);
+        break;
+    default:
+        break;
+    }
 }
 
 int32_t blinker_main(void* p) {
@@ -130,7 +143,7 @@ int32_t blinker_main(void* p) {
     BlinkerApp* app = malloc(sizeof(BlinkerApp));
     
     // Initialize state
-    app->current_view = BlinkerViewSubmenu;
+    app->current_view = BlinkerViewDialog;  // Changed from Submenu
     app->start_timestamp = 0; // Default value - not important
     app->current_interval = 100; // Default value - not important
     app->duration = 20; // Default value
@@ -141,25 +154,22 @@ int32_t blinker_main(void* p) {
     view_dispatcher_attach_to_gui(app->view_dispatcher, app->gui, ViewDispatcherTypeFullscreen);
 
     // Initialize views
-    app->submenu = submenu_alloc();
+    app->dialog = dialog_ex_alloc();  // Changed from submenu
     app->widget = widget_alloc();
-
-    // Initialize number input
     app->number_input = number_input_alloc();
-    view_dispatcher_add_view(
-        app->view_dispatcher,
-        BlinkerViewNumber,
-        number_input_get_view(app->number_input));
-    
-    
 
-    // Add menu items
-    submenu_add_item(app->submenu, "Start", 1, start_blink_callback, app);
-    submenu_add_item(app->submenu, "Set Duration", 0, set_duration_callback, app);
-    
+    // Configure dialog
+    dialog_ex_set_context(app->dialog, app);
+    dialog_ex_set_result_callback(app->dialog, dialog_callback);
+    // Ensure dialog has a reference to app context for back button
     view_dispatcher_set_event_callback_context(app->view_dispatcher, app);
-    view_dispatcher_add_view(app->view_dispatcher, BlinkerViewSubmenu, submenu_get_view(app->submenu));
+    view_dispatcher_set_navigation_event_callback(app->view_dispatcher, back_button);
+    update_main_view(app);
+
+    // Add views
+    view_dispatcher_add_view(app->view_dispatcher, BlinkerViewDialog, dialog_ex_get_view(app->dialog));
     view_dispatcher_add_view(app->view_dispatcher, BlinkerViewWidget, widget_get_view(app->widget));
+    view_dispatcher_add_view(app->view_dispatcher, BlinkerViewNumber, number_input_get_view(app->number_input));
 
     // On back button press
     view_dispatcher_set_navigation_event_callback(app->view_dispatcher, back_button);
@@ -168,16 +178,16 @@ int32_t blinker_main(void* p) {
     app->timer = furi_timer_alloc(timer_callback, FuriTimerTypePeriodic, app);
 
     // Run application
-    view_dispatcher_switch_to_view(app->view_dispatcher, BlinkerViewSubmenu);
+    view_dispatcher_switch_to_view(app->view_dispatcher, BlinkerViewDialog);
     view_dispatcher_run(app->view_dispatcher);
 
     // Cleanup
     furi_timer_free(app->timer);
     view_dispatcher_remove_view(app->view_dispatcher, BlinkerViewWidget);
-    view_dispatcher_remove_view(app->view_dispatcher, BlinkerViewSubmenu);
+    view_dispatcher_remove_view(app->view_dispatcher, BlinkerViewDialog);
     view_dispatcher_remove_view(app->view_dispatcher, BlinkerViewNumber);
     widget_free(app->widget);
-    submenu_free(app->submenu);
+    dialog_ex_free(app->dialog);
     number_input_free(app->number_input);
     view_dispatcher_free(app->view_dispatcher);
     furi_record_close(RECORD_GUI);
