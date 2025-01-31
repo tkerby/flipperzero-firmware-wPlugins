a70ff4f33c1c2df43f8cbc308a648f506cc4375d
Patrick762 <lutzpatti86@gmail.com>
2025-01-31 13:25:07 +0100

    basic structure

---
+++ src/nfc_hid.c
@@ -0,0 +1,10 @@
+#include "nfc_hid.h"
+
+/** Entrypoint */
+int32_t nfc_hid_app(void* p) {
+    UNUSED(p);
+
+
+
+    return 0;
+}
---
+++ src/nfc_hid.h
@@ -0,0 +1,8 @@
+#ifndef NFC_HID_HEADERS
+#define NFC_HID_HEADERS
+
+#include "flipper.h"
+
+int32_t nfc_hid_app(void* p);
+
+#endif
