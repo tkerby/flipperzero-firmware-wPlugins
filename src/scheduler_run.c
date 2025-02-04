b981e96300d3878df1db5c6963e93bda97162cf6
Patrick Edwards <shalebridge@gmail.com>
2025-02-04 12:06:56 -0600

    Cleaned up transmission alloc/dealloc.

--- src/scheduler_run.c
+++ src/scheduler_run.c
@@ -13,6 +13,45 @@
 
 #define TAG "SubGhzSchedulerRun"
 
+struct ScheduleTxRun {
+    Storage* storage;
+    FlipperFormat* fff_head;
+    FlipperFormat* fff_file;
+    FlipperFormat* fff_data;
+    SubGhzEnvironment* environment;
+    FuriString* protocol;
+    FuriString* preset;
+    FuriString* data;
+    bool filetype;
+    uint32_t frequency;
+};
+
+static ScheduleTxRun* tx_run_alloc() {
+    ScheduleTxRun* tx_run = malloc(sizeof(ScheduleTxRun));
+    tx_run->storage = furi_record_open(RECORD_STORAGE);
+    tx_run->fff_head = flipper_format_file_alloc(tx_run->storage);
+    tx_run->fff_file = flipper_format_file_alloc(tx_run->storage);
+    tx_run->fff_data = flipper_format_string_alloc();
+    tx_run->environment = subghz_environment_alloc();
+    tx_run->protocol = furi_string_alloc();
+    tx_run->preset = furi_string_alloc();
+    tx_run->data = furi_string_alloc();
+    return tx_run;
+}
+
+static void tx_run_free(ScheduleTxRun* tx_run) {
+    flipper_format_free(tx_run->fff_file);
+    flipper_format_file_close(tx_run->fff_head);
+    flipper_format_free(tx_run->fff_head);
+    flipper_format_free(tx_run->fff_data);
+    furi_string_free(tx_run->preset);
+    furi_string_free(tx_run->protocol);
+    furi_string_free(tx_run->data);
+    subghz_environment_free(tx_run->environment);
+    furi_record_close(RECORD_STORAGE);
+    free(tx_run);
+}
+
 static FuriHalSubGhzPreset scheduler_get_subghz_preset_name(const char* preset_name) {
     FuriHalSubGhzPreset preset = FuriHalSubGhzPresetIDLE;
     if(!strcmp(preset_name, "FuriHalSubGhzPresetOok270Async")) {
@@ -53,80 +92,68 @@ static void
 static int32_t scheduler_tx(void* context) {
     furi_assert(context);
     SchedulerApp* app = context;
-    Storage* storage = furi_record_open(RECORD_STORAGE);
-    FlipperFormat* fff_head = flipper_format_file_alloc(storage);
-    FlipperFormat* fff_file = flipper_format_file_alloc(storage);
-    FlipperFormat* fff_data = flipper_format_string_alloc();
-    SubGhzEnvironment* environment = subghz_environment_alloc();
-    FuriString* protocol = furi_string_alloc();
-    FuriString* preset = furi_string_alloc();
-    FuriString* data = furi_string_alloc();
-    bool filetype = scheduler_get_file_type(app->scheduler);
-    uint32_t frequency;
+    ScheduleTxRun* tx_run = tx_run_alloc();
 
     const SubGhzDevice* device = subghz_devices_get_by_name(SUBGHZ_DEVICE_CC1101_INT_NAME);
     subghz_devices_begin(device);
     subghz_devices_reset(device);
 
-    if(filetype == SchedulerFileTypePlaylist) {
-        flipper_format_file_open_existing(fff_head, furi_string_get_cstr(app->file_path));
-        flipper_format_read_string(fff_head, "sub", data);
+    tx_run->filetype = scheduler_get_file_type(app->scheduler);
+    if(tx_run->filetype == SchedulerFileTypePlaylist) {
+        flipper_format_file_open_existing(tx_run->fff_head, furi_string_get_cstr(app->file_path));
+        flipper_format_read_string(tx_run->fff_head, "sub", tx_run->data);
     } else {
-        furi_string_set_str(data, furi_string_get_cstr(app->file_path));
+        furi_string_set_str(tx_run->data, furi_string_get_cstr(app->file_path));
     }
 
     do {
-        if(!flipper_format_file_open_existing(fff_file, furi_string_get_cstr(data))) {
+        if(!flipper_format_file_open_existing(
+               tx_run->fff_file, furi_string_get_cstr(tx_run->data))) {
             FURI_LOG_E(TAG, "Error loading file!");
             return -1;
         }
 
-        flipper_format_read_uint32(fff_file, "Frequency", &frequency, 1);
-        flipper_format_read_string(fff_file, "Preset", preset);
-        flipper_format_read_string(fff_file, "Protocol", protocol);
+        flipper_format_read_uint32(tx_run->fff_file, "Frequency", &tx_run->frequency, 1);
+        flipper_format_read_string(tx_run->fff_file, "Preset", tx_run->preset);
+        flipper_format_read_string(tx_run->fff_file, "Protocol", tx_run->protocol);
 
-        if(furi_string_equal(protocol, "RAW")) {
+        if(furi_string_equal(tx_run->protocol, "RAW")) {
             subghz_protocol_raw_gen_fff_data(
-                fff_data, furi_string_get_cstr(data), subghz_devices_get_name(device));
+                tx_run->fff_data,
+                furi_string_get_cstr(tx_run->data),
+                subghz_devices_get_name(device));
         } else {
             stream_copy_full(
-                flipper_format_get_raw_stream(fff_file), flipper_format_get_raw_stream(fff_data));
+                flipper_format_get_raw_stream(tx_run->fff_file),
+                flipper_format_get_raw_stream(tx_run->fff_data));
         }
-        flipper_format_file_close(fff_file);
+        flipper_format_file_close(tx_run->fff_file);
 
-        subghz_environment_set_protocol_registry(environment, (void*)&subghz_protocol_registry);
-        SubGhzTransmitter* transmitter =
-            subghz_transmitter_alloc_init(environment, furi_string_get_cstr(protocol));
+        subghz_environment_set_protocol_registry(
+            tx_run->environment, (void*)&subghz_protocol_registry);
+        SubGhzTransmitter* transmitter = subghz_transmitter_alloc_init(
+            tx_run->environment, furi_string_get_cstr(tx_run->protocol));
 
-        subghz_transmitter_deserialize(transmitter, fff_data);
+        subghz_transmitter_deserialize(transmitter, tx_run->fff_data);
 
         FuriHalSubGhzPreset preset_enum =
-            scheduler_get_subghz_preset_name(furi_string_get_cstr(preset));
+            scheduler_get_subghz_preset_name(furi_string_get_cstr(tx_run->preset));
         subghz_devices_load_preset(device, preset_enum, NULL);
 
-        frequency = subghz_devices_set_frequency(device, frequency);
+        tx_run->frequency = subghz_devices_set_frequency(device, tx_run->frequency);
 
         transmit(app, device, transmitter);
 
         subghz_transmitter_free(transmitter);
-        if(filetype == SchedulerFileTypePlaylist) {
+        if(tx_run->filetype == SchedulerFileTypePlaylist) {
             furi_delay_ms(500);
         }
-    } while(filetype == SchedulerFileTypePlaylist &&
-            flipper_format_read_string(fff_head, "sub", data));
+    } while(tx_run->filetype == SchedulerFileTypePlaylist &&
+            flipper_format_read_string(tx_run->fff_head, "sub", tx_run->data));
 
     subghz_devices_sleep(device);
     subghz_devices_end(device);
-
-    flipper_format_free(fff_file);
-    flipper_format_file_close(fff_head);
-    flipper_format_free(fff_head);
-    flipper_format_free(fff_data);
-    furi_string_free(preset);
-    furi_string_free(protocol);
-    furi_string_free(data);
-    subghz_environment_free(environment);
-    furi_record_close(RECORD_STORAGE);
+    tx_run_free(tx_run);
 
     return 0;
 }
--- src/scheduler_run.h
+++ src/scheduler_run.h
@@ -3,5 +3,7 @@
 #include <flipper_format_i.h>
 #include "scheduler_app_i.h"
 
+typedef struct ScheduleTxRun ScheduleTxRun;
+
 //FuriThreadCallback scheduler_tx(SchedulerApp* context);
 void scheduler_start_tx(SchedulerApp* context);
