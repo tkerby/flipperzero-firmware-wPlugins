674d84a2a1e853df1cab4b5c35101327899fc6c8
jay candel <creepersbeast@gmail.com>
2025-02-06 00:23:04 +0800

    add skinny cigarette

--- flipper_cigarette.c
+++ flipper_cigarette.c
@@ -33,6 +33,7 @@ static ImagePosition smoke_position = {.x = 0, .y = 0};
 typedef struct {
     int32_t counter, maxdrags;
     bool lit, done;
+    uint8_t style; // 0 = regular, 1 = skinny
     IconAnimation* icon;
     NotificationApp* notification;
 } Cigarette;
@@ -75,7 +76,7 @@ static void app_draw_callback(Canvas* canvas, void* ctx) {
     if(stats_view.visible) {
         char buffer[64];
         // 1 cig in app = 15 IRL cigs (one pack)
-        // 14.7% cancer risk after 20 years of 15/day (nhs data)
+        // 14.7% cancer risk after 20 years of 15/day
         float cancer_pct = (stats_view.total_smokes * 15.0f) / (15 * 365 * 20) * 14.7f;
         if(cancer_pct > 100.0f) 
             cancer_pct = 100.0f;
@@ -105,8 +106,10 @@ static void app_draw_callback(Canvas* canvas, void* ctx) {
         canvas_draw_icon_animation(canvas, smoke_position.x, smoke_position.y, cigarette.icon);
     }
 
-    if (cigarette.lit == false && cigarette.done == false)
+    if (cigarette.lit == false && cigarette.done == false) {
         elements_button_center(canvas, "light up");
+        canvas_draw_str_aligned(canvas, 64, 15, AlignCenter, AlignCenter, "< switch style >");
+    }
 
     if (cigarette.done == true) {
         elements_button_left(canvas, "another one");
@@ -115,55 +118,57 @@ static void app_draw_callback(Canvas* canvas, void* ctx) {
     // TODO: open about-view
     //elements_button_down(canvas, "about");
 
-    // Cigarette
+    // Cigarette icon selection based on style
+    const Icon* current_icon = NULL;
     switch(cigarette.counter) {
         case 0:
-            canvas_draw_icon(canvas, image_position.x, image_position.y, &I_cigarette0_115x25);
+            current_icon = (cigarette.style == 0) ? &I_cigarette0_115x25 : &I_skinny0_112x20;
             break;
         case 1:
-            canvas_draw_icon(canvas, image_position.x, image_position.y, &I_cigarette1_115x25);
+            current_icon = (cigarette.style == 0) ? &I_cigarette1_115x25 : &I_skinny1_112x20;
             break;
         case 2:
-            canvas_draw_icon(canvas, image_position.x, image_position.y, &I_cigarette2_115x25);
+            current_icon = (cigarette.style == 0) ? &I_cigarette2_115x25 : &I_skinny2_112x20;
             break;
         case 3:
-            canvas_draw_icon(canvas, image_position.x, image_position.y, &I_cigarette3_115x25);
+            current_icon = (cigarette.style == 0) ? &I_cigarette3_115x25 : &I_skinny3_112x20;
             break;
         case 4:
-            canvas_draw_icon(canvas, image_position.x, image_position.y, &I_cigarette4_115x25);
+            current_icon = (cigarette.style == 0) ? &I_cigarette4_115x25 : &I_skinny4_112x20;
             break;
         case 5:
-            canvas_draw_icon(canvas, image_position.x, image_position.y, &I_cigarette5_115x25);
+            current_icon = (cigarette.style == 0) ? &I_cigarette5_115x25 : &I_skinny5_112x20;
             break;
         case 6:
-            canvas_draw_icon(canvas, image_position.x, image_position.y, &I_cigarette6_115x25);
+            current_icon = (cigarette.style == 0) ? &I_cigarette6_115x25 : &I_skinny6_112x20;
             break;
         case 7:
-            canvas_draw_icon(canvas, image_position.x, image_position.y, &I_cigarette7_115x25);
+            current_icon = (cigarette.style == 0) ? &I_cigarette7_115x25 : &I_skinny7_112x20;
             break;
         case 8:
-            canvas_draw_icon(canvas, image_position.x, image_position.y, &I_cigarette8_115x25);
+            current_icon = (cigarette.style == 0) ? &I_cigarette8_115x25 : &I_skinny8_112x20;
             break;
         case 9:
-            canvas_draw_icon(canvas, image_position.x, image_position.y, &I_cigarette9_115x25);
+            current_icon = (cigarette.style == 0) ? &I_cigarette9_115x25 : &I_skinny9_112x20;
             break;
         case 10:
-            canvas_draw_icon(canvas, image_position.x, image_position.y, &I_cigarette10_115x25);
+            current_icon = (cigarette.style == 0) ? &I_cigarette10_115x25 : &I_skinny10_112x20;
             break;
         case 11:
-            canvas_draw_icon(canvas, image_position.x, image_position.y, &I_cigarette11_115x25);
+            current_icon = (cigarette.style == 0) ? &I_cigarette11_115x25 : &I_skinny11_112x20;
             break;
         case 12:
-            canvas_draw_icon(canvas, image_position.x, image_position.y, &I_cigarette12_115x25);
+            current_icon = (cigarette.style == 0) ? &I_cigarette12_115x25 : &I_skinny12_112x20;
             break;
         case 13:
-            canvas_draw_icon(canvas, image_position.x, image_position.y, &I_cigarette13_115x25);
+            current_icon = (cigarette.style == 0) ? &I_cigarette13_115x25 : &I_skinny13_112x20;
             cigarette.done = true;
             break;
         default:
-            canvas_draw_icon(canvas, image_position.x, image_position.y, &I_cigarette0_115x25);
+            current_icon = &I_cigarette0_115x25;
             break;
     }
+    canvas_draw_icon(canvas, image_position.x, image_position.y, current_icon);
 }
 
 static void app_input_callback(InputEvent* input_event, void* ctx) {
@@ -208,18 +213,24 @@ int32_t cigarette_main(void* p) {
             if((event.type == InputTypePress) || (event.type == InputTypeRepeat)) {
                 switch(event.key) {
                 case InputKeyLeft:
-                    if(cigarette.done) {
+                    if(!stats_view.visible && !cigarette.lit && !cigarette.done) {
+                        // Cycle through styles
+                        cigarette.style = (cigarette.style - 1 + 2) % 2;
+                    } else if(cigarette.done) {
                         stats_view.total_smokes++;
+                        cigarette.counter = 0;
+                        cigarette.lit = false;
+                        cigarette.done = false;
+                        smoke_position.x = 0;
+                        image_position.x = 12;
+                        icon_animation_stop(cigarette.icon);
                     }
-                    cigarette.counter = 0;
-                    cigarette.lit = false;
-                    cigarette.done = false;
-                    smoke_position.x = 0;
-                    image_position.x = 12;
-                    icon_animation_stop(cigarette.icon);
                     break;
                 case InputKeyRight:
-                    if (cigarette.lit && !cigarette.done) {
+                    if(!stats_view.visible && !cigarette.lit && !cigarette.done) {
+                        // Cycle through styles
+                        cigarette.style = (cigarette.style + 1) % 2;
+                    } else if (cigarette.lit && !cigarette.done) {
                         cigarette.counter++;
                         notification_message(cigarette.notification, &sequence_puff);
                         smoke_position.x += 5;
@@ -237,7 +248,6 @@ int32_t cigarette_main(void* p) {
                     }
                     break;
                 case InputKeyOk:
-
                     if (cigarette.counter == 0 && !cigarette.lit) {
                         cigarette.lit = true;
                         notification_message(cigarette.notification, &sequence_light_up);
