0ff1de4d527b207ad93588009f9ed4fc20a4882d
RogueMaster <RogueMaster@users.noreply.github.com>
2025-02-06 15:53:13 -0500

    Latest RM0205-1648-0.420.0-2f4a2d0 on PATREON - ADD PORTAL OF FLIPPER

--- applications/external/portal_of_flipper/portal_of_flipper.c
+++ applications/external/portal_of_flipper/portal_of_flipper.c
@@ -39,6 +39,7 @@ PoFApp* pof_app_alloc() {
 
     // View Dispatcher
     app->view_dispatcher = view_dispatcher_alloc();
+    view_dispatcher_enable_queue(app->view_dispatcher);
     app->scene_manager = scene_manager_alloc(&pof_scene_handlers, app);
 
     view_dispatcher_set_event_callback_context(app->view_dispatcher, app);
