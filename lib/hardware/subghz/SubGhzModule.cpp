803e442c079c5ecee667bf63145c584b5942083b
Denis Radolin <denr01@yandex-team.ru>
2025-03-05 23:25:48 +0300

    subghz wip

--- lib/hardware/subghz/SubGhzModule.cpp
+++ lib/hardware/subghz/SubGhzModule.cpp
@@ -18,14 +18,38 @@
 
 #include "lib/HandlerContext.cpp"
 
+#undef LOG_TAG
+#define LOG_TAG "SUB_GHZ"
+
 class SubGhzModule {
 private:
     SubGhzEnvironment* environment;
     const SubGhzDevice* device;
+    SubGhzReceiver* receiver;
+
     bool isExternal;
     uint32_t frequency = 433920000;
 
-    static receiveCallback(void* context) {
+    static void receiveCallback(bool level, uint32_t duration, void* context) {
+        UNUSED(level);
+        UNUSED(duration);
+        UNUSED(context);
+    }
+
+    static void captureCallback(SubGhzReceiver* receiver, SubGhzProtocolDecoderBase* decoderBase, void* context) {
+        if(context == NULL) {
+            FURI_LOG_W(LOG_TAG, "SubGhz module has NULL receive handler!");
+            return;
+        }
+
+        FuriString* text = furi_string_alloc();
+        subghz_protocol_decoder_base_get_string(decoderBase, text);
+        subghz_receiver_reset(receiver);
+        printf("%s", furi_string_get_cstr(text));
+        furi_string_free(text);
+
+        HandlerContext<function<void()>>* handlerContext = (HandlerContext<function<void()>>*)context;
+        handlerContext->GetHandler()();
     }
 
 public:
@@ -49,24 +73,38 @@ public:
         subghz_devices_load_preset(device, FuriHalSubGhzPresetOok650Async, NULL);
 
         SetFrequency(frequency);
+
+        receiver = subghz_receiver_alloc_init(environment);
+        subghz_receiver_set_filter(receiver, SubGhzProtocolFlag_Decodable);
     }
 
     uint32_t SetFrequency(uint32_t frequency) {
         return subghz_devices_set_frequency(device, frequency);
     }
 
-    void ReceiveAsync(function<void()> handler) {
-        subghz_devices_start_async_rx(device, receiveCallback, new HandlerContext(handler));
+    void SetReceiveHandler(function<void()> handler) {
+        subghz_receiver_set_rx_callback(receiver, captureCallback, new HandlerContext(handler));
+    }
+
+    void ReceiveAsync() {
+        subghz_devices_start_async_rx(device, (void*)receiveCallback, NULL);
     }
 
     ~SubGhzModule() {
         if(furi_hal_power_is_otg_enabled()) {
             furi_hal_power_disable_otg();
         }
+
+        if(receiver != NULL) {
+            subghz_receiver_free(receiver);
+            receiver = NULL;
+        }
+
         if(environment != NULL) {
             subghz_environment_free(environment);
             environment = NULL;
         }
+
         if(device != NULL) {
             subghz_devices_end(device);
             subghz_devices_deinit();
