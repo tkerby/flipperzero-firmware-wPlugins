48981e6ae89b880812ed2847912d5fd7bf72cf3a
RogueMaster <RogueMaster@users.noreply.github.com>
2025-03-16 20:52:18 -0400

    Latest RM0314-1528-0.420.0-86a8013 on PATREON - UPD SUBGHz

--- lib/subghz/protocols/revers_rb2.c
+++ lib/subghz/protocols/revers_rb2.c
@@ -45,10 +45,12 @@ const SubGhzProtocolDecoder subghz_protocol_revers_rb2_decoder = {
     .feed = subghz_protocol_decoder_revers_rb2_feed,
     .reset = subghz_protocol_decoder_revers_rb2_reset,
 
-    .get_hash_data = subghz_protocol_decoder_revers_rb2_get_hash_data,
+    .get_hash_data = NULL,
+    .get_hash_data_long = subghz_protocol_decoder_revers_rb2_get_hash_data,
     .serialize = subghz_protocol_decoder_revers_rb2_serialize,
     .deserialize = subghz_protocol_decoder_revers_rb2_deserialize,
     .get_string = subghz_protocol_decoder_revers_rb2_get_string,
+    .get_string_brief = NULL,
 };
 
 const SubGhzProtocolEncoder subghz_protocol_revers_rb2_encoder = {
@@ -364,10 +366,10 @@ void subghz_protocol_decoder_revers_rb2_feed(void* context, bool level, volatile
     }
 }
 
-uint8_t subghz_protocol_decoder_revers_rb2_get_hash_data(void* context) {
+uint32_t subghz_protocol_decoder_revers_rb2_get_hash_data(void* context) {
     furi_assert(context);
     SubGhzProtocolDecoderRevers_RB2* instance = context;
-    return subghz_protocol_blocks_get_hash_data(
+    return subghz_protocol_blocks_get_hash_data_long(
         &instance->decoder, (instance->decoder.decode_count_bit / 8) + 1);
 }
 
--- lib/subghz/protocols/revers_rb2.h
+++ lib/subghz/protocols/revers_rb2.h
@@ -78,7 +78,7 @@ void subghz_protocol_decoder_revers_rb2_feed(void* context, bool level, uint32_t
  * @param context Pointer to a SubGhzProtocolDecoderRevers_RB2 instance
  * @return hash Hash sum
  */
-uint8_t subghz_protocol_decoder_revers_rb2_get_hash_data(void* context);
+uint32_t subghz_protocol_decoder_revers_rb2_get_hash_data(void* context);
 
 /**
  * Serialize data SubGhzProtocolDecoderRevers_RB2.
