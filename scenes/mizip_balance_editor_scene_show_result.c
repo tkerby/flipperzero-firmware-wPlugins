8edd710c28bccc8ce0570dc0f9300d6b61f930ba
Téo Humeau <teo.humeau@outlook.com>
2025-03-21 16:31:34 +0100

    Showing UID as header

--- scenes/mizip_balance_editor_scene_show_result.c
+++ scenes/mizip_balance_editor_scene_show_result.c
@@ -11,6 +11,19 @@ void mizip_balance_editor_update_balance(void* context) {
     FuriString* message;
     dialog_ex_reset(app->dialog_ex);
     if(app->is_valid_mizip_file) {
+        //Get and show UID
+        char uid[18];
+        snprintf(
+            uid,
+            sizeof(uid),
+            "UID: %02X %02X %02X %02X",
+            app->mf_classic_data->block[0].data[0],
+            app->mf_classic_data->block[0].data[1],
+            app->mf_classic_data->block[0].data[2],
+            app->mf_classic_data->block[0].data[3]);
+        dialog_ex_set_header(app->dialog_ex, uid, 64, 0, AlignCenter, AlignTop);
+
+        //Get and show current balance
         if(app->mf_classic_data->block[10].data[0] == 0x55) {
             app->credit_pointer = 0x09;
         } else {
@@ -18,7 +31,6 @@ void mizip_balance_editor_update_balance(void* context) {
         }
         app->current_balance = (app->mf_classic_data->block[app->credit_pointer].data[2] << 8) |
                                (app->mf_classic_data->block[app->credit_pointer].data[1]);
-
         char str[50];
         snprintf(
             str,
@@ -26,6 +38,8 @@ void mizip_balance_editor_update_balance(void* context) {
             "Current balance: %d.%02d E",
             app->current_balance / 100,
             app->current_balance % 100);
+
+        //Init message and buttons
         message = furi_string_alloc_set(str);
         dialog_ex_set_left_button_text(app->dialog_ex, "-");
         dialog_ex_set_right_button_text(app->dialog_ex, "+");
@@ -36,8 +50,6 @@ void mizip_balance_editor_update_balance(void* context) {
     }
     dialog_ex_set_text(
         app->dialog_ex, furi_string_get_cstr(message), 64, 29, AlignCenter, AlignCenter);
-    dialog_ex_set_header(
-        app->dialog_ex, furi_string_get_cstr(app->filePath), 64, 0, AlignCenter, AlignTop);
     dialog_ex_set_result_callback(
         app->dialog_ex, mizip_balance_editor_scene_confirm_dialog_callback);
     dialog_ex_set_context(app->dialog_ex, app);
