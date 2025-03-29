7714c1a049a491fdaf1b197db620f8feb3768f16
RogueMaster <RogueMaster@users.noreply.github.com>
2025-03-29 17:07:50 -0400

    Squashed 'applications/external/' changes from 2eb5a5ef41..681cf58575
    681cf58575 Latest RM0322-2301-0.420.0-145d3cc on PATREON - FMT
    ab205640c6 Latest RM0322-2301-0.420.0-145d3cc on PATREON - UPD NFC MAKER
    1368af52be Latest RM0322-2301-0.420.0-145d3cc on PATREON - 4x MFW
    dda20af6d8 Latest RM0322-2301-0.420.0-145d3cc on PATREON - Changes From Willy-JL
    6b63a0238f Merge applications/external updates from jamisonderek
    aa4abf8c44 Squashed 'applications/external/' changes from d56fdf50bf..2eb5a5ef41
    
    git-subtree-dir: applications/external
    git-subtree-split: 681cf58575c4c2510f4a6d572978966b9adb956c

--- applications/external/nfc_maker/scenes/nfc_maker_scene_contact_mail.c
+++ applications/external/nfc_maker/scenes/nfc_maker_scene_contact_mail.c
@@ -28,6 +28,8 @@ void nfc_maker_scene_contact_mail_on_enter(void* context) {
 
     nfc_maker_text_input_set_minimum_length(text_input, 0);
 
+    nfc_maker_text_input_show_illegal_symbols(text_input, true);
+
     view_dispatcher_switch_to_view(app->view_dispatcher, NfcMakerViewTextInput);
 }
 
