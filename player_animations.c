1a326bd6282c6017c2279b49aeee93f3f454fe6c
ratmanZorry <ratmanzorry@gmail.com>
2025-03-29 18:40:31 +0300

    added flipped animation voids (without breaking everything)

--- player_animations.c
+++ player_animations.c
@@ -4,9 +4,17 @@ Sprite* idle[2];
 Sprite* sword_swing[7];
 Sprite* walking[8];
 
+Sprite* idle_right[2];
+Sprite* sword_swing_right[7];
+Sprite* walking_right[8];
+
 int idle_current_frame = 0;
 int swinging_sword_current_frame = 0;
 int walking_current_frame = 0;
+
+int idle_right_current_frame = 0;
+int swinging_sword_right_current_frame = 0;
+int walking_right_current_frame = 0;
 
 // this tells how many frames to wait until going to the next animation frame
 int idle_fps = 30;
@@ -16,6 +24,10 @@ int walking_fps = 4;
 int idle_i;
 int sword_i;
 int walking_i;
+
+int idle_right_i;
+int sword_right_i;
+int walking_right_i;
 
 void Idle_animation_load(GameManager* manager) {
     idle[0] = game_manager_sprite_load(manager, "other/player.fxbm");
@@ -41,6 +53,32 @@ void Walking_animation_load(GameManager* manager) {
     walking[5] = game_manager_sprite_load(manager, "walking/walking_5.fxbm");
     walking[6] = game_manager_sprite_load(manager, "walking/walking_6.fxbm");
     walking[7] = game_manager_sprite_load(manager, "walking/walking_7.fxbm");
+}
+
+// facing right animation load
+void Idle_animation_right_load(GameManager* manager) {
+    idle_right[0] = game_manager_sprite_load(manager, "other/player_right.fxbm");
+    idle_right[1] = game_manager_sprite_load(manager, "other/idle_right.fxbm");
+}
+
+void Swinging_sword_right_animation_load(GameManager *manager){
+    sword_swing_right[0] = game_manager_sprite_load(manager, "swinging_sword/swinging_sword_right_1.png");
+    sword_swing_right[2] = game_manager_sprite_load(manager, "swinging_sword/swinging_sword_right_2.png");
+    sword_swing_right[2] = game_manager_sprite_load(manager, "swinging_sword/swinging_sword_right_3.png");
+    sword_swing_right[3] = game_manager_sprite_load(manager, "swinging_sword/swinging_sword_right_4.png");
+    sword_swing_right[4] = game_manager_sprite_load(manager, "swinging_sword/swinging_sword_right_5.png");
+    sword_swing_right[5] = game_manager_sprite_load(manager, "swinging_sword/swinging_sword_right_6.png");
+}
+
+void Walking_right_animation_load(GameManager* manager) {
+    walking_right[0] = game_manager_sprite_load(manager, "walking/walking_right_0.png");
+    walking_right[1] = game_manager_sprite_load(manager, "walking/walking_right_1.png");
+    walking_right[2] = game_manager_sprite_load(manager, "walking/walking_right_2.png");
+    walking_right[3] = game_manager_sprite_load(manager, "walking/walking_right_3.png");
+    walking_right[4] = game_manager_sprite_load(manager, "walking/walking_right_4.png");
+    walking_right[5] = game_manager_sprite_load(manager, "walking/walking_right_5.png");
+    walking_right[6] = game_manager_sprite_load(manager, "walking/walking_right_6.png");
+    walking_right[7] = game_manager_sprite_load(manager, "walking/walking_right_7.png");
 }
 
 void Idle_animation_play(GameManager* manager, void* context) {
--- player_animations.h
+++ player_animations.h
@@ -8,12 +8,24 @@ extern Sprite* idle[];
 extern Sprite* sword_swing[];
 extern Sprite* walking[];
 
+extern Sprite* idle_right[];
+extern Sprite* sword_swing_right[];
+extern Sprite* walking_right[];
+
 void Idle_animation_load(GameManager* manager);
 void Swinging_sword_animation_load(GameManager* manager);
 void Walking_animation_load(GameManager* manager);
 
+void Idle_animation_right_load(GameManager* manager);
+void Swinging_sword_right_animation_load(GameManager* manager);
+void Walking_right_animation_load(GameManager* manager);
+
 void Idle_animation_play(GameManager* manager, void* context);
 void Swinging_sword_animation_play(GameManager* manager, void* context);
 void Walking_animation_play(GameManager* manager, void* context);
+
+void Idle_animation_right_play(GameManager* manager, void* context);
+void Swinging_sword_animation_right_play(GameManager* manager, void* context);
+void Walking_right_animation_play(GameManager* manager, void* context);
 
 #endif
