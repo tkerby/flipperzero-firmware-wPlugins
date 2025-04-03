0283cf07163aebf7b46338f41a5cdc748c581055
RogueMaster <RogueMaster@users.noreply.github.com>
2025-04-03 17:51:54 -0400

    Latest RM0401-2258-0.420.0-26d3544 on PATREON - ADD UART PONG

--- applications/external/uart_pong/uart_pong.c
+++ applications/external/uart_pong/uart_pong.c
@@ -8,16 +8,16 @@ static void render_callback(Canvas* canvas, void* ctx) {
     canvas_clear(canvas);
 
     canvas_draw_box(canvas, 0, app->me.y, 3, 12);
-    canvas_draw_box(canvas, 128-3, app->enemy.y, 3, 12);
+    canvas_draw_box(canvas, 128 - 3, app->enemy.y, 3, 12);
 
-    canvas_draw_box(canvas, (int32_t) app->ball.x, (int32_t) app->ball.y, 3, 3);
+    canvas_draw_box(canvas, (int32_t)app->ball.x, (int32_t)app->ball.y, 3, 3);
 
     char buf[32];
     snprintf(buf, sizeof(buf), "%d : %d", app->me.score, app->enemy.score);
     canvas_draw_str_aligned(canvas, 64, 0, AlignCenter, AlignTop, buf);
 
-    float deg = atan2(app->ball.dy, app->ball.dx) * (double) (180.0 / M_PI);
-    snprintf(buf, sizeof(buf), "angle: %.2f", (double) deg);
+    float deg = atan2(app->ball.dy, app->ball.dx) * (double)(180.0 / M_PI);
+    snprintf(buf, sizeof(buf), "angle: %.2f", (double)deg);
     canvas_draw_str_aligned(canvas, 64, 10, AlignCenter, AlignTop, buf);
 }
 
@@ -29,7 +29,7 @@ static void input_callback(InputEvent* input_event, void* ctx) {
 
 static void uart_callback(FuriHalSerialHandle* handle, FuriHalSerialRxEvent event, void* ctx) {
     furi_assert(ctx);
-    UartPongApp* app = ctx;  
+    UartPongApp* app = ctx;
     UNUSED(app);
 
     if(event == FuriHalSerialRxEventData) {
@@ -44,28 +44,23 @@ static void game_tick(void* ctx) {
 
     uint8_t prev_y = app->me.y;
 
-    if(app->controls.up && app->me.y > 0)
-        app->me.y--;
+    if(app->controls.up && app->me.y > 0) app->me.y--;
 
-    if(app->controls.down && app->me.y < 63-12)
-        app->me.y++;
-        
-    if(prev_y != app->me.y)
-        furi_hal_serial_tx(app->handle, &app->me.y, 1);
+    if(app->controls.down && app->me.y < 63 - 12) app->me.y++;
+
+    if(prev_y != app->me.y) furi_hal_serial_tx(app->handle, &app->me.y, 1);
 
     app->ball.y += app->ball.dy;
     app->ball.x += app->ball.dx;
 
-    if(app->ball.y < 1 || app->ball.y > 63-3)
-        app->ball.dy *= -1;
+    if(app->ball.y < 1 || app->ball.y > 63 - 3) app->ball.dy *= -1;
 
-    if(app->ball.x < 1 || app->ball.x > 127-3)
-        app->ball.dx *= -1;
+    if(app->ball.x < 1 || app->ball.x > 127 - 3) app->ball.dx *= -1;
 
     if(app->ball.x <= 3 && app->ball.y > app->me.y && app->ball.y < app->me.y + 12) {
         // ToDo:
     }
-    
+
     view_port_update(app->view_port);
 }
 
@@ -87,12 +82,12 @@ static UartPongApp* uart_pong_alloc() {
     furi_hal_serial_async_rx_start(app->handle, uart_callback, app, true);
 
     app->timer = furi_timer_alloc(game_tick, FuriTimerTypePeriodic, app);
-    furi_timer_start(app->timer, furi_ms_to_ticks(1000/FPS));
+    furi_timer_start(app->timer, furi_ms_to_ticks(1000 / FPS));
     furi_timer_set_thread_priority(FuriTimerThreadPriorityElevated);
 
     app->ball.x = 32.f;
     app->ball.y = 16.f;
-    
+
     float angle = degToRad(45.f);
     app->ball.dx = (float)cos(angle) * SPEED;
     app->ball.dy = (float)sin(angle) * SPEED;
@@ -131,16 +126,14 @@ int32_t uart_pong_app(void* p) {
                 break;
             };
 
-            if (event.key == InputKeyUp && event.type == InputTypePress)
-                app->controls.up = true;
-            
-            if (event.key == InputKeyDown && event.type == InputTypePress)
+            if(event.key == InputKeyUp && event.type == InputTypePress) app->controls.up = true;
+
+            if(event.key == InputKeyDown && event.type == InputTypePress)
                 app->controls.down = true;
 
-            if (event.key == InputKeyUp && event.type == InputTypeRelease)
-                app->controls.up = false;
+            if(event.key == InputKeyUp && event.type == InputTypeRelease) app->controls.up = false;
 
-            if (event.key == InputKeyDown && event.type == InputTypeRelease)
+            if(event.key == InputKeyDown && event.type == InputTypeRelease)
                 app->controls.down = false;
         }
     }
