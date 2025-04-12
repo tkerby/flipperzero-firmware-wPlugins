f4aefbfb9cd2cca9af9aaf951be2fabcbb7a5dc8
ratmanZorry <ratmanzorry@gmail.com>
2025-04-12 09:56:33 +0300

    moved update block down

--- player.c
+++ player.c
@@ -5,7 +5,7 @@ bool isGrounded;
 bool isJumping;
 
 bool is_moving;
-bool can_move = true;
+bool can_move = false;
 
 bool is_facing_right;
 
@@ -35,71 +35,8 @@ void player_spawn(Level* level, GameManager* manager) {
     Swinging_sword_right_animation_load(manager);
 }
 
-void player_update(Entity* self, GameManager* manager, void* context) {
-    PlayerContext* playerContext = (PlayerContext*)context;
-    GameContext* game_context = game_manager_game_context_get(manager);
-    InputState input = game_manager_input_get(manager);
+void player_movement() {
 
-    Vector pos = entity_pos_get(self);
-
-    // isGrounded check
-    if((pos.y + 12) >= game_context->ground_hight && playerContext->Yvelocity >= 0) {
-        pos.y = game_context->ground_hight - 12;
-        playerContext->Yvelocity = 0;
-        isGrounded = true;
-    } else {
-        isGrounded = false;
-    }
-
-    // gravity
-    if(!isGrounded) {
-        playerContext->Yvelocity += 0.5;
-        pos.y += playerContext->Yvelocity;
-    }
-
-    if(can_move){
-        if(input.held & GameKeyLeft) pos.x -= 1;
-        if(input.held & GameKeyRight) pos.x += 1;        
-    }
-
-    if(input.pressed & GameKeyOk){
-        playerContext->is_swinging_sword = true;
-    }
-
-    if(playerContext->is_swinging_sword) can_move = false;
-    else can_move = true;
-
-    if((input.held & GameKeyLeft || input.held & GameKeyRight) && can_move)
-        is_moving = true;
-    else
-        is_moving = false;
-
-    if(input.held & GameKeyLeft){
-        is_facing_right = false;
-    }
-    
-    if(input.held & GameKeyRight){
-        is_facing_right = true;
-    }
-
-    // jump
-    if(input.pressed & GameKeyUp && isGrounded) {
-        playerContext->Yvelocity = -3.5;
-    }
-
-    // Clamp player position to screen bounds, considering player sprite size (10x10)
-    pos.x = CLAMP(pos.x, 123, 5);
-    pos.y = CLAMP(pos.y, 59, 5);
-
-    // Set new player position
-    entity_pos_set(self, pos);
-
-    // Control game exit
-    if(input.pressed & GameKeyBack) {
-        game_manager_game_stop(manager);
-    }
-
-    Animations(manager, context);
 }
 
 void player_collision(Entity* self, Entity* other, GameManager* manager, void* context) {
@@ -181,6 +118,76 @@ void Animations(GameManager* manager, void* context) {
 
     UNUSED(manager);
 }
+
+void player_update(Entity* self, GameManager* manager, void* context) {
+    PlayerContext* playerContext = (PlayerContext*)context;
+    GameContext* game_context = game_manager_game_context_get(manager);
+    InputState input = game_manager_input_get(manager);
+    
+    if(input.pressed & GameKeyOk){
+        playerContext->is_swinging_sword = true;
+    }
+
+    can_move = !playerContext->is_swinging_sword;
+
+    Vector pos = entity_pos_get(self);
+
+
+    // isGrounded check
+    if((pos.y + 12) >= game_context->ground_hight && playerContext->Yvelocity >= 0) {
+        pos.y = game_context->ground_hight - 12;
+        playerContext->Yvelocity = 0;
+        isGrounded = true;
+    } else {
+        isGrounded = false;
+    }
+
+    // gravity
+    if(!isGrounded) {
+        playerContext->Yvelocity += 0.5;
+        pos.y += playerContext->Yvelocity;
+    }
+    
+    if(can_move){
+        if(input.held & GameKeyLeft) pos.x -= 1;
+        if(input.held & GameKeyRight) pos.x += 1;
+    }
+    
+
+    if((input.held & GameKeyLeft || input.held & GameKeyRight) && can_move)
+        is_moving = true;
+    else
+        is_moving = false;
+
+    if(input.held & GameKeyLeft){
+        is_facing_right = false;
+    }
+    
+    if(input.held & GameKeyRight){
+        is_facing_right = true;
+    }
+
+    // jump
+    if(input.pressed & GameKeyUp && isGrounded) {
+        playerContext->Yvelocity = -3.5;
+    }
+
+    // clamp player position to screen bounds
+    pos.x = CLAMP(pos.x, 123, 5);
+    pos.y = CLAMP(pos.y, 59, 5);
+
+    // set new player position
+    entity_pos_set(self, pos);
+
+    // control game exit
+    if(input.pressed & GameKeyBack) {
+        game_manager_game_stop(manager);
+    }
+
+    player_movement();
+    Animations(manager, context);
+}
+
 
 const EntityDescription player_desc = {
     .start = NULL, // called when entity is added to the level
