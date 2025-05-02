9e99f7e87bf95c779807719ccc8f3ae5d47f26c0
jblanked <82678820+jblanked@users.noreply.github.com>
2025-05-02 09:45:46 -0400

    update Doom_8bit

--- engine/Arduino/vgm_game_engine/examples/Doom_8bit/Doom_8bit.ino
+++ engine/Arduino/vgm_game_engine/examples/Doom_8bit/Doom_8bit.ino
@@ -9,16 +9,27 @@
 - Flash Size: 2MB (Sketch: 1984KB, FS: 64KB)
 - CPU Speed: 200MHz
 */
+auto board = VGMConfig; // Video Game Module Configuration
 void setup()
 {
   // Setup file system (must be called in setup)
   setup_fs();
 
   // Create the game instance with its name, start/stop callbacks, and colors.
-  Game *game = new Game("Doom", Vector(320, 240), NULL, NULL, TFT_WHITE, TFT_BLACK, true);
+  Game *game = new Game(
+      "Doom",                            // Game name
+      Vector(board.width, board.height), // Game size
+      NULL,                              // start callback
+      NULL,                              // stop callback
+      TFT_WHITE,                         // Foreground color
+      TFT_BLACK,                         // Background color
+      true,                              // Use 8-bit graphics?
+      board,                             // Board configuration
+      true                               // Use double buffering for TFT
+  );
 
   // set world size
-  game->world_size = Vector(320, 240);
+  game->world_size = Vector(board.width, board.height);
 
   // UART buttons
   ButtonUART *uart = new ButtonUART();
@@ -27,7 +38,7 @@ void setup()
   game->input_add(new Input(uart));
 
   // Create and add a level to the game.
-  Level *level = new Level("Level 1", Vector(320, 240), game);
+  Level *level = new Level("Level 1", Vector(board.width, board.height), game);
   game->level_add(level);
 
   // Add the player entity to the level
