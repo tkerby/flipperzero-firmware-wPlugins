2de8fef3f9fc4537f48fdfe295761b9316623631
Lizard <li0ard@proton.me>
2025-05-07 00:39:29 +0600

    feat: add C2 alternative for Cyfral CCD 2094 (v2.3, v2.5)

--- ibutton_converter_utils.h
+++ ibutton_converter_utils.h
@@ -8,6 +8,8 @@ void cyfral_to_dallas_c1(uint8_t cyfral_code[2], uint8_t dallas_code[8]);
 
 void cyfral_to_dallas_c2(uint8_t cyfral_code[2], uint8_t dallas_code[8]);
 
+void cyfral_to_dallas_c2_alt(uint8_t cyfral_code[2], uint8_t dallas_code[8]);
+
 void cyfral_to_dallas_c3(uint8_t cyfral_code[2], uint8_t dallas_code[8]);
 
 void cyfral_to_dallas_c4(uint8_t cyfral_code[2], uint8_t dallas_code[8]);
