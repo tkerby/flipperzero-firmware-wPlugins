6ac1ab509cd50374f83ff388d6f6e6ca2468cd5b
Luu <lewisgrafs@gmail.com>
2025-05-22 18:57:39 +0200

    fix clipper date

--- scenes/plugins/clipper.c
+++ scenes/plugins/clipper.c
@@ -542,7 +542,7 @@ static void furi_string_cat_timestamp(
     const char* time_hdr,
     uint32_t tmst_1900) {
     DateTime tm;
-
+    tmst_1900 -= 2208988800;
     datetime_timestamp_to_datetime(tmst_1900, &tm);
 
     FuriString* date_str = furi_string_alloc();
