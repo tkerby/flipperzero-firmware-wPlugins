50ea01f0cf1004876c34eacbeaf9cbe6936cf062
KHOPAN <puthipnakchan@gmail.com>
2025-06-14 17:52:06 +0700

    Test input callback

--- cuberzero.c
+++ cuberzero.c
@@ -9,16 +9,29 @@
 		furi_hal_light_set(LightBlue, blue);   \
 	} while(0)
 
+static void callbackInput(const InputEvent* const event, const PCUBERZERO instance) {
+	furi_message_queue_put(instance->queue, event, 0);
+}
+
+static void callbackRender(const Canvas* const event, const PCUBERZERO context) {
+	UNUSED(event);
+	UNUSED(context);
+}
+
 int32_t cuberzeroMain(const void* const pointer) {
 	UNUSED(pointer);
 	CUBERZERO_LOG("Initializing");
-	FuriMessageQueue* queue = furi_message_queue_alloc(8, sizeof(InputEvent));
-	furi_check(queue, "furi_message_queue_alloc() failed");
+	PCUBERZERO instance = malloc(sizeof(CUBERZERO));
+	instance->queue = furi_message_queue_alloc(8, sizeof(InputEvent));
+	instance->viewport = view_port_alloc();
+	instance->interface = furi_record_open(RECORD_GUI);
+	view_port_input_callback_set(instance->viewport, (ViewPortInputCallback) callbackInput, instance);
+	view_port_draw_callback_set(instance->viewport, (ViewPortDrawCallback) callbackRender, instance);
+	gui_add_view_port(instance->interface, instance->viewport, GuiLayerFullscreen);
+
 	InputEvent event;
 
-	while(1) {
-		furi_check(furi_message_queue_get(queue, &event, FuriWaitForever) == FuriStatusOk, "furi_message_queue_get() failed");
-
+	while(furi_message_queue_get(instance->queue, &event, FuriWaitForever) == FuriStatusOk) {
 		if(event.key == InputKeyOk) {
 			if(event.type == InputTypePress) {
 				LIGHT(255, 0, 0);
@@ -33,7 +46,11 @@ int32_t cuberzeroMain(const void* const pointer) {
 		}
 	}
 
-	furi_message_queue_free(queue);
+	gui_remove_view_port(instance->interface, instance->viewport);
+	furi_record_close(RECORD_GUI);
+	view_port_free(instance->viewport);
+	furi_message_queue_free(instance->queue);
+	free(instance);
 	CUBERZERO_LOG("Exiting");
 	return 0;
 }
--- cuberzero.h
+++ cuberzero.h
@@ -1,3 +1,10 @@
 #include <furi.h>
+#include <gui/gui.h>
 
 #define CUBERZERO_LOG(message) FURI_LOG_I("CuberZero", message)
+
+typedef struct {
+	FuriMessageQueue* queue;
+	ViewPort* viewport;
+	Gui* interface;
+} CUBERZERO, *PCUBERZERO;
