f54c89ae7a68e53f2363a1a2a00d2527a7192250
KHOPAN <puthipnakchan@gmail.com>
2025-06-14 15:16:38 +0700

    Update cuberzero.c

--- cuberzero.c
+++ cuberzero.c
@@ -4,6 +4,8 @@
 int32_t cuberzeroMain(const void* const pointer) {
 	UNUSED(pointer);
 	CUBERZERO_LOG("Initializing");
+	furi_hal_light_set(LightRed, 0);
+	furi_hal_light_set(LightGreen, 0);
 	furi_hal_light_set(LightBlue, 255);
 	return 0;
 }
