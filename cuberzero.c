c39103ec059fe0edd716c38457352d81e7d55466
KHOPAN <puthipnakchan@gmail.com>
2025-06-14 15:22:34 +0700

    Test the onboard LED

--- cuberzero.c
+++ cuberzero.c
@@ -4,6 +4,9 @@
 int32_t cuberzeroMain(const void* const pointer) {
 	UNUSED(pointer);
 	CUBERZERO_LOG("Initializing");
+	furi_hal_light_set(LightRed, 0);
+	furi_hal_light_set(LightGreen, 0);
 	furi_hal_light_set(LightBlue, 255);
+	CUBERZERO_LOG("Exiting");
 	return 0;
 }
