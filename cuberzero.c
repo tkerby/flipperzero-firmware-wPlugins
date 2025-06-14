14647c39673c02bfb179db3264494e916a8fb760
KHOPAN <puthipnakchan@gmail.com>
2025-06-14 14:52:18 +0700

    Test lighting

--- cuberzero.c
+++ cuberzero.c
@@ -1,7 +1,9 @@
 #include "cuberzero.h"
+#include <furi_hal_light.h>
 
 int32_t cuberzeroMain(const void* const pointer) {
 	UNUSED(pointer);
 	CUBERZERO_LOG("Initializing");
+	furi_hal_light_set(LightBlue, 255);
 	return 0;
 }
