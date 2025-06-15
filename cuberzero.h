0c9675d92cd50b7c1ceb56a4449801783bd3e302
KHOPAN <puthipnakchan@gmail.com>
2025-06-15 14:59:10 +0700

    Refactor the project

--- cuberzero.h
+++ cuberzero.h
@@ -1,3 +1,27 @@
 #include <furi.h>
 #include <applications/services/gui/view_dispatcher.h>
 #include <applications/services/gui/scene_manager.h>
+#include <gui/modules/submenu.h>
+
+#define CUBERZERO_TAG "CuberZero"
+
+typedef struct {
+	ViewDispatcher* dispatcher;
+	SceneManager* manager;
+	struct {
+		Submenu* submenu;
+	} view;
+} CUBERZERO, *PCUBERZERO;
+
+typedef enum {
+	CUBERZERO_VIEW_SUBMENU
+} CUBERZEROVIEW;
+
+typedef enum {
+	CUBERZERO_SCENE_HOME,
+	CUBERZERO_SCENE_COUNT
+} CUBERZEROSCENE;
+
+void SceneHomeEnter(const PCUBERZERO instance);
+bool SceneHomeEvent(const PCUBERZERO instance, const SceneManagerEvent event);
+void SceneHomeExit(const PCUBERZERO instance);
--- scene_home.c
+++ scene_home.c
@@ -1,33 +1,17 @@
-/*#include "cuberzero.h"
-#include <gui/modules/submenu.h>
+#include "cuberzero.h"
 
-void SceneHomeInitialize(const PCUBERZERO instance, char** const message) {
-	instance->scene.home = (View*) submenu_alloc();
-
-	if(!instance->scene.home) {
-		*message = "submenu_alloc() failed";
-	}
+void SceneHomeEnter(const PCUBERZERO instance) {
+	submenu_reset(instance->view.submenu);
+	submenu_add_item(instance->view.submenu, "Timer", 0, NULL, instance);
+	view_dispatcher_switch_to_view(instance->dispatcher, CUBERZERO_VIEW_SUBMENU);
 }
 
-void SceneHomeOnEnter(const PCUBERZERO instance) {
-	submenu_reset((Submenu*) instance->scene.home);
-	submenu_set_header((Submenu*) instance->scene.home, "Test Header");
-	//submenu_add_item((Submenu*) instance->scene.home, "Test Item 1", 0, NULL, instance);
-	//submenu_add_item((Submenu*) instance->scene.home, "Test Item 2", 1, NULL, instance);
-	view_dispatcher_switch_to_view(instance->dispatcher, CUBERZERO_SCENE_HOME);
-}
-
-bool SceneHomeOnEvent(const PCUBERZERO instance, const SceneManagerEvent event) {
+bool SceneHomeEvent(const PCUBERZERO instance, const SceneManagerEvent event) {
 	UNUSED(instance);
 	UNUSED(event);
-	return true;
+	return false;
 }
 
-void SceneHomeOnExit(const PCUBERZERO instance) {
+void SceneHomeExit(const PCUBERZERO instance) {
 	UNUSED(instance);
-	//submenu_reset((Submenu*) instance->scene.home);
 }
-
-void SceneHomeFree(const PCUBERZERO instance) {
-	submenu_free((Submenu*) instance->scene.home);
-}*/
