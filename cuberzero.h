3b466080bb464d1da63eaa4f3ec61b0b6e0ed747
KHOPAN <puthipnakchan@gmail.com>
2025-06-15 13:32:32 +0700

    Restarting from scratch

--- cuberzero.h
+++ cuberzero.h
@@ -1,24 +1,3 @@
 #include <furi.h>
 #include <applications/services/gui/view_dispatcher.h>
 #include <applications/services/gui/scene_manager.h>
-
-#define CUBERZERO_LOG(message) FURI_LOG_I("CuberZero", message)
-
-typedef struct {
-	ViewDispatcher* dispatcher;
-	SceneManager* manager;
-	struct {
-		View* home;
-	} scene;
-} CUBERZERO, *PCUBERZERO;
-
-typedef enum {
-	CUBERZERO_SCENE_HOME,
-	CUBERZERO_SCENE_COUNT
-} CUBERZEROSCENE;
-
-void SceneHomeInitialize(const PCUBERZERO instance, char** const message);
-void SceneHomeOnEnter(const PCUBERZERO instance);
-bool SceneHomeOnEvent(const PCUBERZERO instance, const SceneManagerEvent event);
-void SceneHomeOnExit(const PCUBERZERO instance);
-void SceneHomeFree(const PCUBERZERO instance);
--- scene_home.c
+++ scene_home.c
@@ -1,4 +1,4 @@
-#include "cuberzero.h"
+/*#include "cuberzero.h"
 #include <gui/modules/submenu.h>
 
 void SceneHomeInitialize(const PCUBERZERO instance, char** const message) {
@@ -11,21 +11,23 @@ void SceneHomeInitialize(const PCUBERZERO instance, char** const message) {
 
 void SceneHomeOnEnter(const PCUBERZERO instance) {
 	submenu_reset((Submenu*) instance->scene.home);
-	submenu_add_item((Submenu*) instance->scene.home, "Test Item 1", 0, NULL, instance);
-	submenu_add_item((Submenu*) instance->scene.home, "Test Item 2", 1, NULL, instance);
+	submenu_set_header((Submenu*) instance->scene.home, "Test Header");
+	//submenu_add_item((Submenu*) instance->scene.home, "Test Item 1", 0, NULL, instance);
+	//submenu_add_item((Submenu*) instance->scene.home, "Test Item 2", 1, NULL, instance);
 	view_dispatcher_switch_to_view(instance->dispatcher, CUBERZERO_SCENE_HOME);
 }
 
 bool SceneHomeOnEvent(const PCUBERZERO instance, const SceneManagerEvent event) {
 	UNUSED(instance);
 	UNUSED(event);
-	return false;
+	return true;
 }
 
 void SceneHomeOnExit(const PCUBERZERO instance) {
-	submenu_reset((Submenu*) instance->scene.home);
+	UNUSED(instance);
+	//submenu_reset((Submenu*) instance->scene.home);
 }
 
 void SceneHomeFree(const PCUBERZERO instance) {
 	submenu_free((Submenu*) instance->scene.home);
-}
+}*/
