4126d30e0a16e11cd29112abec3d840e0ceff22f
KHOPAN <puthipnakchan@gmail.com>
2025-06-15 18:18:52 +0700

    Test file reading

--- settings.c
+++ settings.c
@@ -1,34 +1,39 @@
 #include "cuberzero.h"
 #include <storage/storage.h>
 
-static const char* const SettingsFile = APP_DATA_PATH("cuberzero_settings.bin");
+static const char* const SettingsFile = APP_DATA_PATH("cuberzero_settings.cbzs");
+
+static void loadDefaultSettings() {
+}
 
 void SettingsLoad() {
 	Storage* storage = furi_record_open(RECORD_STORAGE);
 
 	if(!storage) {
-		goto loadDefaults;
+		loadDefaultSettings();
+		return;
 	}
 
-	if(!storage_file_exists(storage, SettingsFile)) {
-		FURI_LOG_I(CUBERZERO_TAG, "File does not exist");
-		furi_record_close(RECORD_STORAGE);
-		goto loadDefaults;
-	}
-
-	FURI_LOG_I(CUBERZERO_TAG, "File does exist");
 	File* file = storage_file_alloc(storage);
 
 	if(!file) {
-		furi_record_close(RECORD_STORAGE);
-		goto loadDefaults;
+		loadDefaultSettings();
+		goto closeStorage;
 	}
 
-	//storage_file_open(file, SettingsFile, NULL, NULL);
+	if(!storage_file_open(file, SettingsFile, FSAM_READ, FSOM_OPEN_EXISTING)) {
+		loadDefaultSettings();
+		goto freeFile;
+	}
+
+	for(uint64_t i = 0; i < storage_file_size(file); i++) {
+		FURI_LOG_I(CUBERZERO_TAG, "Counter");
+	}
+freeFile:
+	storage_file_close(file);
 	storage_file_free(file);
-	return;
-loadDefaults:
-	return;
+closeStorage:
+	furi_record_close(RECORD_STORAGE);
 }
 
 void SettingsSave() {
