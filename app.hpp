f29f82df8feb4bfb787a64ee471c57ff668ae56b
jblanked <82678820+jblanked@users.noreply.github.com>
2025-07-04 09:35:29 -0400

    bump to 1.3 + check wifi credentials

--- app.hpp
+++ app.hpp
@@ -7,7 +7,7 @@
 #include "about/about.hpp"
 
 #define TAG "FlipDownloader"
-#define VERSION "1.2"
+#define VERSION "1.3"
 #define VERSION_TAG TAG " " VERSION
 #define APP_ID "flip_downloader"
 
@@ -30,20 +30,12 @@ typedef enum
 class FlipDownloaderApp
 {
 private:
-    Submenu *submenu = nullptr;
-    FlipperHTTP *flipperHttp = nullptr;
-
-    // Run class instance
-    std::unique_ptr<FlipDownloaderRun> run;
-
-    // Settings class instance
-    std::unique_ptr<FlipDownloaderSettings> settings;
-
-    // About class instance
-    std::unique_ptr<FlipDownloaderAbout> about;
-
-    // Timer for run updates
-    FuriTimer *timer = nullptr;
+    std::unique_ptr<FlipDownloaderAbout> about;       // About class instance
+    FlipperHTTP *flipperHttp = nullptr;               // FlipperHTTP instance for network requests
+    std::unique_ptr<FlipDownloaderRun> run;           // Run class instance
+    std::unique_ptr<FlipDownloaderSettings> settings; // Settings class instance
+    Submenu *submenu = nullptr;                       // Submenu instance for navigation
+    FuriTimer *timer = nullptr;                       // Timer for run updates
 
     static uint32_t callbackExitApp(void *context);
     void callbackSubmenuChoices(uint32_t index);
@@ -55,16 +47,24 @@ private:
     static void timerCallback(void *context);
 
 public:
+    FlipDownloaderApp();
+    ~FlipDownloaderApp();
+    //
     Gui *gui = nullptr;
     ViewDispatcher *viewDispatcher = nullptr;
     ViewPort *viewPort = nullptr;
     //
+    void freeRunView(); // free the Run View
     size_t getBytesReceived() const noexcept { return flipperHttp ? flipperHttp->bytes_received : 0; }
     size_t getContentLength() const noexcept { return flipperHttp ? flipperHttp->content_length : 0; }
     HTTPState getHttpState() const noexcept { return flipperHttp ? flipperHttp->state : INACTIVE; }
+    bool hasWiFiCredentials();                                             // check if WiFi credentials are set
+    bool initRunView();                                                    // initialize the Run View
     bool isBoardConnected();                                               // check if the board is connected
     bool load_char(const char *path_name, char *value, size_t value_size); // load a string from storage
-    bool save_char(const char *path_name, const char *value);              // save a string to storage
+    void runDispatcher();
+    bool save_char(const char *path_name, const char *value);         // save a string to storage
+    bool sendWiFiCredentials(const char *ssid, const char *password); // send WiFi credentials to the board
     bool setHttpState(HTTPState state = IDLE) noexcept
     {
         if (flipperHttp)
@@ -74,6 +74,8 @@ public:
         }
         return false;
     }
+    static void viewPortDraw(Canvas *canvas, void *context);
+    static void viewPortInput(InputEvent *event, void *context);
 
     bool httpDownloadFile(
         const char *saveLocation, // full path where the file will be saved
@@ -97,12 +99,4 @@ public:
         HTTPMethod method = GET,
         const char *headers = "{\"Content-Type\": \"application/json\"}",
         const char *payload = nullptr);
-
-    FlipDownloaderApp();
-    ~FlipDownloaderApp();
-
-    void runDispatcher();
-
-    static void viewPortDraw(Canvas *canvas, void *context);
-    static void viewPortInput(InputEvent *event, void *context);
 };
