52ca0546181dbfcbbbdc60b45c8ff586673bda6c
jblanked <82678820+jblanked@users.noreply.github.com>
2025-07-05 18:08:42 -0400

    merge some methods from Free Roam

--- FlipperZero/src/run/run.cpp
+++ FlipperZero/src/run/run.cpp
@@ -11,6 +11,48 @@ FlipWorldRun::~FlipWorldRun()
     // nothing to do
 }
 
+void FlipWorldRun::debounceInput()
+{
+    static uint8_t debounceCounter = 0;
+    if (shouldDebounce)
+    {
+        lastInput = InputKeyMAX;
+        debounceCounter++;
+        if (debounceCounter < 2)
+        {
+            return;
+        }
+        debounceCounter = 0;
+        shouldDebounce = false;
+        inputHeld = false;
+    }
+}
+
+void FlipWorldRun::inputManager()
+{
+    static int inputHeldCounter = 0;
+
+    // Track input held state
+    if (lastInput != InputKeyMAX)
+    {
+        inputHeldCounter++;
+        if (inputHeldCounter > 10)
+        {
+            this->inputHeld = true;
+        }
+    }
+    else
+    {
+        inputHeldCounter = 0;
+        this->inputHeld = false;
+    }
+
+    if (shouldDebounce)
+    {
+        debounceInput();
+    }
+}
+
 void FlipWorldRun::updateDraw(Canvas *canvas)
 {
     canvas_clear(canvas);
--- FlipperZero/src/run/run.hpp
+++ FlipperZero/src/run/run.hpp
@@ -6,12 +6,28 @@ class FlipWorldApp;
 
 class FlipWorldRun
 {
-    bool shouldReturnToMenu = false; // Flag to signal return to menu
+private:
+    std::unique_ptr<Draw> draw;         // Draw instance
+    std::unique_ptr<GameEngine> engine; // Engine instance
+    bool inputHeld = false;             // Flag to check if input is held
+    bool isGameRunning = false;         // Flag to check if the game is running
+    InputKey lastInput = InputKeyMAX;   // Last input key pressed
+    bool shouldReturnToMenu = false;    // Flag to signal return to menu
+    //
+    int atoi(const char *nptr) { return (int)strtol(nptr, NULL, 10); } // convert string to integer
+    void debounceInput();                                              // debounce input to prevent multiple actions from a single press
+    void inputManager();                                               // manage input for the game, called from updateInput
 public:
     FlipWorldRun();
     ~FlipWorldRun();
+    bool shouldDebounce = false; // public for Player access
     //
-    bool isActive() const { return shouldReturnToMenu == false; } // Check if the run is active
+    InputKey getCurrentInput() const { return lastInput; }        // Get the last input key pressed
+    GameEngine *getEngine() const { return engine.get(); }        // Get the game engine instance
+    Draw *getDraw() const { return draw.get(); }                  // Get the Draw instance
+    bool isActive() const { return shouldReturnToMenu == false; } // Check if the game is active
+    bool isRunning() const { return isGameRunning; }              // Check if the game engine is running
+    void resetInput() { lastInput = InputKeyMAX; }                // Reset input after processing
     void updateDraw(Canvas *canvas);                              // update and draw the run
     void updateInput(InputEvent *event);                          // update input for the run
 };
