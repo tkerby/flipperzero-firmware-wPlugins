19c26498fe99ca58cd39304072fa857c7ca3f1ea
KHOPAN <puthipnakchan@gmail.com>
2025-07-13 17:17:04 +0700

    Update scene_session_select.c

--- src/scene/scene_session_select.c
+++ src/scene/scene_session_select.c
@@ -10,8 +10,12 @@ static void callbackDraw(Canvas* const canvas, void* const context) {
 	furi_check(canvas);
 	UNUSED(context);
 	canvas_clear(canvas);
-	elements_text_box(canvas, 10, 10, 50, 50, AlignLeft, AlignTop, "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \" de Finibus Bonorum et Malorum \" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \" Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.", 1);
-	elements_frame(canvas, 10, 10, 50, 50);
+	canvas_set_font(canvas, FontPrimary);
+	canvas_set_color(canvas, ColorBlack);
+	canvas_draw_str(canvas, 1, 9, "Current Session:");
+	canvas_draw_line(canvas, 1, 10, 126, 10);
+	//elements_text_box(canvas, 10, 10, 50, 50, AlignLeft, AlignTop, "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \" de Finibus Bonorum et Malorum \" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \" Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.", 1);
+	//elements_frame(canvas, 10, 10, 50, 50);
 }
 
 static void callbackInput(InputEvent* const event, void* const context) {
