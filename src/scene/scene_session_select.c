66f190ba600616684bdd06c5fd4c0701f24f686e
KHOPAN <puthipnakchan@gmail.com>
2025-07-14 23:10:56 +0700

    Test drawing text

--- src/scene/scene_session_select.c
+++ src/scene/scene_session_select.c
@@ -36,13 +36,11 @@ static void callbackDraw(Canvas* const canvas, void* const context) {
 	furi_check(canvas);
 	UNUSED(context);
 	canvas_clear(canvas);
-	canvas_set_font(canvas, FontSecondary);
 	canvas_set_color(canvas, ColorBlack);
-	//canvas_draw_str(canvas, 1, 9, "Current Session:");
-	//canvas_draw_line(canvas, 1, 10, 126, 10);
-	//elements_text_box(canvas, 10, 10, 50, 50, AlignLeft, AlignTop, "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \" de Finibus Bonorum et Malorum \" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \" Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.", 1);
-	//elements_frame(canvas, 10, 10, 50, 50);
-	canvas_draw_str(canvas, 0, 8, "Test Text, Hello, world!");
+	canvas_set_font(canvas, FontPrimary);
+	canvas_draw_str(canvas, 0, 8, "Current Session:");
+	//canvas_draw_box(canvas, 0, 9, 128, 41);
+	elements_text_box(canvas, 0, 11, 128, 39, AlignLeft, AlignTop, "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \" de Finibus Bonorum et Malorum \" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \" Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.", 1);
 	canvas_set_font(canvas, FontSecondary);
 	drawButton(canvas, 10, 51, 1, "Select");
 	drawButton(canvas, 52, 51, 0, "New");
