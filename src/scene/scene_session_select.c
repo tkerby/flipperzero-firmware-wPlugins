bc88a0caf621a95f32d4a9f41fc8e89f4c2a18e4
KHOPAN <puthipnakchan@gmail.com>
2025-07-14 22:56:24 +0700

    Remove dynamic position calculation

--- src/scene/scene_session_select.c
+++ src/scene/scene_session_select.c
@@ -16,7 +16,7 @@ enum SESSIONSELECTBUTTON {
 	BUTTPN_DELETE
 };
 
-static uint8_t drawButton(Canvas* const canvas, const uint8_t x, const uint8_t y, const uint8_t pressed, const char* const text) {
+static void drawButton(Canvas* const canvas, const uint8_t x, const uint8_t y, const uint8_t pressed, const char* const text) {
 	canvas_set_font(canvas, FontSecondary);
 	const uint16_t width = canvas_string_width(canvas, text);
 	canvas_set_color(canvas, ColorBlack);
@@ -31,7 +31,6 @@ static uint8_t drawButton(Canvas* const canvas, const uint8_t x, const uint8_t y
 	}
 
 	canvas_draw_str(canvas, x + 3, y + 10, text);
-	return (uint8_t) width;
 }
 
 static void callbackDraw(Canvas* const canvas, void* const context) {
@@ -46,10 +45,9 @@ static void callbackDraw(Canvas* const canvas, void* const context) {
 	//elements_text_box(canvas, 10, 10, 50, 50, AlignLeft, AlignTop, "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \" de Finibus Bonorum et Malorum \" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \" Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.", 1);
 	//elements_frame(canvas, 10, 10, 50, 50);
 	canvas_draw_str(canvas, 0, 8, "Test Text, Hello, world!");
-	const double_t cellWidth = ((double_t) (110.0 - canvas_string_width(canvas, TEXT_SELECT) - canvas_string_width(canvas, TEXT_NEW) - canvas_string_width(canvas, TEXT_DELETE))) / ((double_t) 4.0);
-	uint8_t width = drawButton(canvas, (uint8_t) cellWidth, 51, 1, "Select");
-	width += drawButton(canvas, ((uint8_t) (cellWidth * ((double_t) 2.0))) + width, 51, 0, "New");
-	drawButton(canvas, ((uint8_t) (cellWidth * ((double_t) 3.0))) + width, 51, 1, "Delete");
+	drawButton(canvas, 10, 51, 1, "Select");
+	drawButton(canvas, 52, 51, 0, "New");
+	drawButton(canvas, 86, 51, 1, "Delete");
 }
 
 static void callbackInput(InputEvent* const event, void* const context) {
