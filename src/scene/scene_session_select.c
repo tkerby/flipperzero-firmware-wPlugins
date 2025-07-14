a3c3549ab3651ddeb1abd207d96bbea48b041eee
KHOPAN <puthipnakchan@gmail.com>
2025-07-14 23:00:21 +0700

    Update scene_session_select.c

--- src/scene/scene_session_select.c
+++ src/scene/scene_session_select.c
@@ -16,8 +16,7 @@ enum SESSIONSELECTBUTTON {
 	BUTTPN_DELETE
 };
 
-static void drawButton(Canvas* const canvas, const uint8_t x, const uint8_t y, const uint8_t pressed, const char* const text) {
-	canvas_set_font(canvas, FontSecondary);
+static inline void drawButton(Canvas* const canvas, const uint8_t x, const uint8_t y, const uint8_t pressed, const char* const text) {
 	const uint16_t width = canvas_string_width(canvas, text);
 	canvas_set_color(canvas, ColorBlack);
 	canvas_draw_line(canvas, x + 1, y, x + width + 4, y);
@@ -37,7 +36,6 @@ static void callbackDraw(Canvas* const canvas, void* const context) {
 	furi_check(canvas);
 	UNUSED(context);
 	canvas_clear(canvas);
-	drawButton(canvas, 20, 20, 0, "Delete");
 	canvas_set_font(canvas, FontSecondary);
 	canvas_set_color(canvas, ColorBlack);
 	//canvas_draw_str(canvas, 1, 9, "Current Session:");
@@ -45,9 +43,10 @@ static void callbackDraw(Canvas* const canvas, void* const context) {
 	//elements_text_box(canvas, 10, 10, 50, 50, AlignLeft, AlignTop, "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \" de Finibus Bonorum et Malorum \" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \" Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.", 1);
 	//elements_frame(canvas, 10, 10, 50, 50);
 	canvas_draw_str(canvas, 0, 8, "Test Text, Hello, world!");
+	canvas_set_font(canvas, FontSecondary);
 	drawButton(canvas, 10, 51, 1, "Select");
 	drawButton(canvas, 52, 51, 0, "New");
-	drawButton(canvas, 86, 51, 1, "Delete");
+	drawButton(canvas, 86, 51, 0, "Delete");
 }
 
 static void callbackInput(InputEvent* const event, void* const context) {
