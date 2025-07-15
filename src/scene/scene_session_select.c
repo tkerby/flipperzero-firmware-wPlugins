c713124b3e990993ff40776a611335ad2ce4ea7c
KHOPAN <puthipnakchan@gmail.com>
2025-07-15 10:35:36 +0700

    Update scene_session_select.c

--- src/scene/scene_session_select.c
+++ src/scene/scene_session_select.c
@@ -31,39 +31,45 @@ static inline void drawButton(Canvas* const canvas, const uint8_t x, const uint8
 	canvas_draw_str(canvas, x + 3, y + 10, text);
 }
 
-static void callbackDraw(Canvas* const canvas, void* const context) {
+static void callbackRender(Canvas* const canvas, void* const context) {
 	furi_check(canvas);
+	furi_check(context);
+	const PSESSIONSELECTSCENE instance = context;
 	canvas_clear(canvas);
-	canvas_set_color(canvas, ColorBlack);
 	canvas_set_font(canvas, FontPrimary);
 	canvas_draw_str(canvas, 0, 8, "Current Session:");
 	elements_text_box(canvas, 0, 11, 128, 39, AlignLeft, AlignTop, "3x3 Blindfolded Test 2025 at my home, this is a very long session name", 1);
 	canvas_set_font(canvas, FontSecondary);
-	drawButton(canvas, 10, 51, ((PSESSIONSELECTSCENE) context)->selectedButton == BUTTON_SELECT, "Select");
-	drawButton(canvas, 52, 51, ((PSESSIONSELECTSCENE) context)->selectedButton == BUTTON_NEW, "New");
-	drawButton(canvas, 86, 51, ((PSESSIONSELECTSCENE) context)->selectedButton == BUTTON_DELETE, "Delete");
+	drawButton(canvas, 10, 51, instance->selectedButton == BUTTON_SELECT, "Select");
+	drawButton(canvas, 52, 51, instance->selectedButton == BUTTON_NEW, "New");
+	drawButton(canvas, 86, 51, instance->selectedButton == BUTTON_DELETE, "Delete");
 }
 
 static void callbackInput(InputEvent* const event, void* const context) {
-	if(!event || !context || event->type != InputTypeShort) {
+	furi_check(event);
+	furi_check(context);
+
+	if(event->type != InputTypeShort) {
 		return;
 	}
+
+	const PSESSIONSELECTSCENE instance = context;
 
 	switch(event->key) {
 	case InputKeyUp:
 	case InputKeyRight:
-		((PSESSIONSELECTSCENE) context)->selectedButton = (((PSESSIONSELECTSCENE) context)->selectedButton + 1) % COUNT_BUTTON;
-		view_port_update(((PSESSIONSELECTSCENE) context)->viewport);
+		instance->selectedButton = (instance->selectedButton + 1) % COUNT_BUTTON;
+		view_port_update(instance->viewport);
 		break;
 	case InputKeyDown:
 	case InputKeyLeft:
-		((PSESSIONSELECTSCENE) context)->selectedButton = (((PSESSIONSELECTSCENE) context)->selectedButton ? ((PSESSIONSELECTSCENE) context)->selectedButton : COUNT_BUTTON) - 1;
-		view_port_update(((PSESSIONSELECTSCENE) context)->viewport);
+		instance->selectedButton = (instance->selectedButton ? instance->selectedButton : COUNT_BUTTON) - 1;
+		view_port_update(instance->viewport);
 		break;
 	case InputKeyOk:
 		break;
 	default:
-		furi_message_queue_put(((PSESSIONSELECTSCENE) context)->queue, event, FuriWaitForever);
+		furi_message_queue_put(instance->queue, event, FuriWaitForever);
 		break;
 	}
 }
@@ -74,22 +80,22 @@ void SceneSessionSelectEnter(void* const context) {
 	instance->instance = context;
 	instance->viewport = view_port_alloc();
 	instance->queue = furi_message_queue_alloc(1, sizeof(InputEvent));
-	view_port_draw_callback_set(instance->viewport, callbackDraw, instance);
+	view_port_draw_callback_set(instance->viewport, callbackRender, instance);
 	view_port_input_callback_set(instance->viewport, callbackInput, instance);
-	view_dispatcher_stop(((PCUBERZERO) context)->dispatcher);
-	gui_remove_view_port(((PCUBERZERO) context)->interface, ((PCUBERZERO) context)->dispatcher->viewport);
-	gui_add_view_port(((PCUBERZERO) context)->interface, instance->viewport, GuiLayerFullscreen);
+	view_dispatcher_stop(instance->instance->dispatcher);
+	gui_remove_view_port(instance->instance->interface, instance->instance->dispatcher->viewport);
+	gui_add_view_port(instance->instance->interface, instance->viewport, GuiLayerFullscreen);
 	const InputEvent* event;
 
 	while(furi_message_queue_get(instance->queue, &event, FuriWaitForever) == FuriStatusOk) {
 		break;
 	}
 
-	gui_remove_view_port(((PCUBERZERO) context)->interface, instance->viewport);
-	gui_add_view_port(((PCUBERZERO) context)->interface, ((PCUBERZERO) context)->dispatcher->viewport, GuiLayerFullscreen);
+	gui_remove_view_port(instance->instance->interface, instance->viewport);
+	gui_add_view_port(instance->instance->interface, instance->instance->dispatcher->viewport, GuiLayerFullscreen);
 	furi_message_queue_free(instance->queue);
 	view_port_free(instance->viewport);
+	scene_manager_handle_back_event(instance->instance->manager);
+	view_dispatcher_run(instance->instance->dispatcher);
 	free(instance);
-	scene_manager_handle_back_event(((PCUBERZERO) context)->manager);
-	view_dispatcher_run(((PCUBERZERO) context)->dispatcher);
 }
