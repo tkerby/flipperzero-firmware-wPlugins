09a32217ffe9ce7c3b2a956f49a167b3d8a03787
KHOPAN <puthipnakchan@gmail.com>
2025-07-23 22:26:37 +0700

    Refactor scene_session_select.c

--- src/scene/scene_session_select.c
+++ src/scene/scene_session_select.c
@@ -15,7 +15,7 @@ enum SESSIONSELECTBUTTON {
 	COUNT_BUTTON
 };
 
-/*static inline void drawButton(Canvas* const canvas, const uint8_t x, const uint8_t y, const uint8_t pressed, const char* const text) {
+static inline void drawButton(Canvas* const canvas, const uint8_t x, const uint8_t y, const uint8_t pressed, const char* const text) {
 	const uint16_t width = canvas_string_width(canvas, text);
 	canvas_set_color(canvas, ColorBlack);
 	canvas_draw_line(canvas, x + 1, y, x + width + 4, y);
@@ -38,7 +38,7 @@ static void callbackRender(Canvas* const canvas, void* const context) {
 	canvas_clear(canvas);
 	canvas_set_font(canvas, FontPrimary);
 	canvas_draw_str(canvas, 0, 8, "Current Session:");
-	elements_text_box(canvas, 0, 11, 128, 39, AlignLeft, AlignTop, "3x3 Blindfolded Test 2025 at my home, this is a very long session name", 1);
+	elements_text_box(canvas, 0, 11, 128, 39, AlignLeft, AlignTop, "Ahhhhhhhhhhhhhhhhh, long tttttttttteeeeeeeeeexxxxxxxxxxtttttttttt", 1);
 	canvas_set_font(canvas, FontSecondary);
 	drawButton(canvas, 10, 51, instance->selectedButton == BUTTON_SELECT, "Select");
 	drawButton(canvas, 52, 51, instance->selectedButton == BUTTON_NEW, "New");
@@ -67,33 +67,21 @@ static void callbackInput(InputEvent* const event, void* const context) {
 		view_port_update(instance->viewport);
 		break;
 	case InputKeyOk:
-		switch(instance->selectedButton) {
-		case BUTTON_SELECT:
-			break;
-		case BUTTON_NEW:
-			break;
-		case BUTTON_DELETE:
-			break;
-		}
-
 		break;
 	default:
 		furi_message_queue_put(instance->queue, event, FuriWaitForever);
 		break;
 	}
-}*/
+}
 
 void SceneSessionSelectEnter(void* const context) {
 	furi_check(context);
-	/*const PSESSIONSELECTSCENE instance = malloc(sizeof(SESSIONSELECTSCENE));
+	const PSESSIONSELECTSCENE instance = malloc(sizeof(SESSIONSELECTSCENE));
 	instance->instance = context;
 	instance->viewport = view_port_alloc();
 	instance->queue = furi_message_queue_alloc(1, sizeof(InputEvent));
-	FuriString* string = furi_string_alloc();
-	FileBrowser* browser = file_browser_alloc(string);
 	view_port_draw_callback_set(instance->viewport, callbackRender, instance);
 	view_port_input_callback_set(instance->viewport, callbackInput, instance);
-	//view_dispatcher_stop(instance->instance->dispatcher);
 	gui_remove_view_port(instance->instance->interface, instance->instance->dispatcher->viewport);
 	gui_add_view_port(instance->instance->interface, instance->viewport, GuiLayerFullscreen);
 	const InputEvent* event;
@@ -104,12 +92,8 @@ void SceneSessionSelectEnter(void* const context) {
 
 	gui_remove_view_port(instance->instance->interface, instance->viewport);
 	gui_add_view_port(instance->instance->interface, instance->instance->dispatcher->viewport, GuiLayerFullscreen);
-	file_browser_free(browser);
-	furi_string_free(string);
 	furi_message_queue_free(instance->queue);
 	view_port_free(instance->viewport);
 	scene_manager_handle_back_event(instance->instance->manager);
-	//view_dispatcher_run(instance->instance->dispatcher);
-	free(instance);*/
-	//file_browser_configure(((PCUBERZERO) context)->view.browser, 0, "/", 0, 0, 0, 0);
+	free(instance);
 }
