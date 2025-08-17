b0e245d265c4df14b7029b34c0c8bd69a32094ad
47LeCoste <82815207+grugnoymeme@users.noreply.github.com>
2025-08-17 11:31:10 +0200

    Update nfc_dict_manager.h

--- nfc_dict_manager.h
+++ nfc_dict_manager.h
@@ -17,7 +17,8 @@
 #include <notification/notification_messages.h>
 #include <datetime/datetime.h>
 #include <toolbox/stream/file_stream.h>
-#include "assets_icons.h"
+//#include "assets_icons.h"
+#include <gui/icon.h> 
 
 #define TAG "NFC_Dict_Manager"
 #define DICT_FOLDER_PATH "/ext/nfc/dictionaries"
