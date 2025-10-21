d0099902c68460daebf1f0c88241e9095b09be04
Haw8411 <109001610+Haw8411@users.noreply.github.com>
2025-10-20 22:10:15 -0400

    Refactor includes and add conditional icon header
    Removed unused includes and added conditional inclusion for ble_spam_icons.h.

--- protocols/_base.h
+++ protocols/_base.h
@@ -1,21 +1,16 @@
+// protocols/_base.h
 #pragma once
 
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-//#include <assets_icons.h>
-#include "ble_spam_icons.h"
-#include <furi_hal_random.h>
-#include <core/core_defines.h>
-#include "../ble_spam.h"
+#include <gui/icon.h>  // brings in 'typedef struct Icon Icon;' from SDK
 
-typedef struct Payload Payload;
-
-typedef struct {
-    const Icon* icon;
-    const char* (*get_name)(const Payload* payload);
-    void (*make_packet)(uint8_t* _size, uint8_t** _packet, Payload* payload);
-    void (*extra_config)(Ctx* ctx);
-    uint8_t (*config_count)(const Payload* payload);
-} Protocol;
+// --- Make generated icons header optional on CI ---
+#if defined(__has_include)
+#  if __has_include("ble_spam_icons.h")
+#    include "ble_spam_icons.h"
+#    define MB_HAS_ICONS 1
+#  else
+#    define MB_HAS_ICONS 0
+#  endif
+#else
+#  define MB_HAS_ICONS 0
+#endif
