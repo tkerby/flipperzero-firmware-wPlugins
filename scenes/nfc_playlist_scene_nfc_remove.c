d1f7bbca8bf5add2ae2f840ef54c2516d8dabe4a
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2025-12-11 16:30:28 +0000

    Quality of life

--- scenes/nfc_playlist_scene_nfc_remove.c
+++ scenes/nfc_playlist_scene_nfc_remove.c
@@ -78,7 +78,7 @@ bool nfc_playlist_nfc_remove_scene_on_event(void* context, SceneManagerEvent eve
    bool consumed = false;
    if(event.type == SceneManagerEventTypeCustom) {
       switch(event.event) {
-      case NfcPlaylistNfcRemove_RemoveLine:
+      case NfcPlaylistNfcRemove_RemoveLine: {
          Storage* storage = furi_record_open(RECORD_STORAGE);
          Stream* stream = file_stream_alloc(storage);
 
@@ -127,7 +127,8 @@ bool nfc_playlist_nfc_remove_scene_on_event(void* context, SceneManagerEvent eve
          }
 
          consumed = true;
-         break;
+         break;         
+      }
       default:
          break;
       }
