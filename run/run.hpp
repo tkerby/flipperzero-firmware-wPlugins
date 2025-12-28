2b333f4c6b93014946e8242adb3d0cbe2708d81e
jblanked <82678820+jblanked@users.noreply.github.com>
2025-12-27 20:49:45 -0500

    remove unused

--- run/run.hpp
+++ run/run.hpp
@@ -112,7 +112,6 @@ class FlipDownloaderRun
     void drawMenuESP32(Canvas *canvasx);                                                                // Draw the firmware menu
     void drawMenuVGM(Canvas *canvasx);                                                                  // Draw the VGM menu
     void drawApps(Canvas *canvas);                                                                      // Draw the apps view
-    void drawGitHubInput(Canvas *canvas);                                                               // Draw GitHub input screen
     void drawGitHubProgress(Canvas *canvas);                                                            // Draw GitHub download progress
     bool downloadApp(const char *appId, const char *buildId, const char *category);                     // Download an app based on the category and index
     bool downloadCategoryInfo(FlipperAppCategory category, uint8_t iteration);                          // Download category information based on the category and iteration
