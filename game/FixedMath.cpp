d16d417c42d4d947db1c96b9dce763ba6d1dabca
apfxtech <aperturefoxtechnology@gmail.com>
2025-12-31 17:44:02 +0300

    (GAME): random update

--- game/FixedMath.cpp
+++ game/FixedMath.cpp
@@ -1,28 +1,16 @@
-#include "FixedMath.h"
+//#include "FixedMath.h"
 
-uint16_t RandomOld()
-{
-	static uint16_t randVal = 0xABC;
+// static uint16_t xs = 1;
 
-	uint16_t lsb = randVal & 1;
-	randVal >>= 1;
-	if (lsb == 1)
-		randVal ^= 0xB400u;
+// uint16_t Random()
+// {
+// 	xs ^= xs << 7;
+// 	xs ^= xs >> 9;
+// 	xs ^= xs << 8;
+// 	return xs;
+// }
 
-	return randVal - 1;
-}
-
-static uint16_t xs = 1;
-
-uint16_t Random()
-{
-	xs ^= xs << 7;
-	xs ^= xs >> 9;
-	xs ^= xs << 8;
-	return xs;
-}
-
-void SeedRandom(uint16_t seed)
-{
-	xs = seed | 1;
-}
+// void SeedRandom(uint16_t seed)
+// {
+// 	xs = seed | 1;
+// }
