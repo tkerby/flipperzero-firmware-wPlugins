8e872da27f29d99be4ff0dd37a07d2c592873fe5
RocketGod <57732082+RocketGod-git@users.noreply.github.com>
2026-01-15 22:33:34 -0800

    Add encoder implementation for Fiat V0 protocol
    Implemented the encoder for the Fiat V0 SubGhz protocol, including allocation, serialization, and signal generation logic. Updated the decoder and related data structures for consistency, improved serialization/deserialization, and added protocol flags for encoding support. Updated README to list Fiat V0 as an encoder.

--- protocols/fiat_v0.h
+++ protocols/fiat_v0.h
@@ -13,9 +13,11 @@
 #define FIAT_PROTOCOL_V0_NAME "Fiat V0"
 
 typedef struct SubGhzProtocolDecoderFiatV0 SubGhzProtocolDecoderFiatV0;
+typedef struct SubGhzProtocolEncoderFiatV0 SubGhzProtocolEncoderFiatV0;
 
 extern const SubGhzProtocol fiat_protocol_v0;
 
+// Decoder functions
 void* subghz_protocol_decoder_fiat_v0_alloc(SubGhzEnvironment* environment);
 void subghz_protocol_decoder_fiat_v0_free(void* context);
 void subghz_protocol_decoder_fiat_v0_reset(void* context);
@@ -28,3 +30,11 @@ SubGhzProtocolStatus subghz_protocol_decoder_fiat_v0_serialize(
 SubGhzProtocolStatus
     subghz_protocol_decoder_fiat_v0_deserialize(void* context, FlipperFormat* flipper_format);
 void subghz_protocol_decoder_fiat_v0_get_string(void* context, FuriString* output);
+
+// Encoder functions
+void* subghz_protocol_encoder_fiat_v0_alloc(SubGhzEnvironment* environment);
+void subghz_protocol_encoder_fiat_v0_free(void* context);
+SubGhzProtocolStatus
+    subghz_protocol_encoder_fiat_v0_deserialize(void* context, FlipperFormat* flipper_format);
+void subghz_protocol_encoder_fiat_v0_stop(void* context);
+LevelDuration subghz_protocol_encoder_fiat_v0_yield(void* context);
