d16059332e7e37a6f88a655c6454030e8f267f3f
RogueMaster <RogueMaster@users.noreply.github.com>
2026-02-16 17:54:33 -0500

    Latest Release on PATREON - FIX

--- applications/external/24cxxprog/i2c_24c02_app.cpp
+++ applications/external/24cxxprog/i2c_24c02_app.cpp
@@ -455,11 +455,12 @@ static void draw_load_file_screen(Canvas* canvas, EEPROMApp* app) {
                     112,
                     filename_str,
                     app->scroll_counter / 5, // Slower scroll speed
+                    false,
                     false);
                 canvas_set_color(canvas, ColorBlack);
             } else {
                 // Normal
-                elements_scrollable_text_line(canvas, 2, y + 5, 112, filename_str, 0, true, true);
+                elements_scrollable_text_line(canvas, 2, y + 5, 112, filename_str, 0, true, false);
             }
 
             furi_string_free(filename_str);
