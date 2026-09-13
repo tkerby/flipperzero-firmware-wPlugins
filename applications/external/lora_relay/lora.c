6c8b8cc81b97a03104284d0cd8119c1f23cb719d
Tim Kerby <git@shutter.pentaprism.org>
2026-09-13 23:22:44 +0100

    Fix const SPI handle in LoRa relay

--- applications/external/lora_relay/lora.c
+++ applications/external/lora_relay/lora.c
@@ -25,7 +25,7 @@ https://github.dev/thekakester/Arduino-LoRa-Sx1262
 
 static uint32_t timeout = 1000;
 // static uint32_t timeout = 100;
-static FuriHalSpiBusHandle* spi = &furi_hal_spi_bus_handle_external;
+static const FuriHalSpiBusHandle* spi = &furi_hal_spi_bus_handle_external;
 
 const GpioPin* const pin_beacon = &gpio_swclk;
 const GpioPin* const pin_nss1 = &gpio_ext_pc0;
